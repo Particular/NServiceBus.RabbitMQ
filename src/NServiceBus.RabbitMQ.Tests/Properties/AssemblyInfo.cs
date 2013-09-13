8d960e61a86fe6df1be92f11dbf57969dc1f9741
Simon Cropp <simon.cropp@gmail.com>
2013-09-13 16:59:46 +1000

    add ripple restore

--- src/NServiceBus.RabbitMQ.Tests/Properties/AssemblyInfo.cs
+++ src/NServiceBus.RabbitMQ.Tests/Properties/AssemblyInfo.cs
@@ -1,9 +1,7 @@
-﻿using System;
-using System.Reflection;
+﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("NServiceBus.RabbitMQ.Tests")]
-[assembly: AssemblyDescription("")]
 [assembly: AssemblyCopyright("Copyright 2010-2013 NServiceBus. All rights reserved")]
 [assembly: AssemblyProduct("NServiceBus.RabbitMQ.Tests")]
 [assembly: AssemblyCompany("NServiceBus Ltd.")]
