d03cfb8fe7382ee7a1960e9dc24bbefc2a0aa89e
Andreas Öhlund <andreas.ohlund@particular.net>
2013-11-29 10:29:45 +0100

    Adding acceptance tests

---
+++ src/NServiceBus.RabbitMQ.AcceptanceTests/App_Packages/NServiceBus.AcceptanceTests/ScenarioDescriptors/SagaPersisters.cs
@@ -0,0 +1,62 @@
+﻿namespace NServiceBus.AcceptanceTests.ScenarioDescriptors
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using AcceptanceTesting.Support;
+    using Persistence.InMemory.SagaPersister;
+    using Persistence.Raven.SagaPersister;
+    using Saga;
+
+    public class SagaPersisters : ScenarioDescriptor
+    {
+        public SagaPersisters()
+        {
+            var persisters = AvailablePeristers;
+
+            foreach (var persister in persisters)
+            {
+                Add(new RunDescriptor
+                {
+                    Key = persister.Name,
+                    Settings = new Dictionary<string, string> { { "SagaPersister", persister.AssemblyQualifiedName } }
+                });
+            }
+        }
+
+        static List<Type> AvailablePeristers
+        {
+            get
+            {
+                var persisters = TypeScanner.GetAllTypesAssignableTo<ISagaPersister>()
+                    .Where(t => !t.IsInterface)
+                    .ToList();
+                return persisters;
+            }
+        }
+
+        public static RunDescriptor Default
+        {
+            get
+            {
+                var persisters = AvailablePeristers;
+                var persister = persisters.FirstOrDefault(p => p != typeof(InMemorySagaPersister) && p != typeof(RavenSagaPersister));
+
+                if (persister == null)
+                {
+                    persister = typeof(InMemorySagaPersister);
+                }
+
+                return new RunDescriptor
+                {
+                    Key = persister.Name,
+                    Settings = new Dictionary<string, string>
+                    {
+                        {"SagaPersister", persister.AssemblyQualifiedName}
+                    }
+                };
+            }
+        }
+    }
+
+}
