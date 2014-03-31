b3239bd76298e83b5dd730fa0b628b5261136736
Panu Peuramäki <peuramaki@iki.fi>
2014-03-31 12:59:04 +0300

    Fix for issue 26 (now UoW works with distributed tx and concurrent operations)

--- src/NServiceBus.RabbitMQ/RabbitMqUnitOfWork.cs
+++ src/NServiceBus.RabbitMQ/RabbitMqUnitOfWork.cs
@@ -1,6 +1,7 @@
 ﻿namespace NServiceBus.Transports.RabbitMQ
 {
     using System;
+    using System.Collections.Concurrent;
     using System.Collections.Generic;
     using System.Linq;
     using System.Text.RegularExpressions;
@@ -35,40 +36,32 @@
             }
 
             var transactionId = transaction.TransactionInformation.LocalIdentifier;
-
-            if (!OutstandingOperations.ContainsKey(transactionId))
+            OutstandingOperations.AddOrUpdate(transactionId, s =>
             {
                 transaction.TransactionCompleted += ExecuteActionsAgainstRabbitMq;
-                OutstandingOperations.Add(transactionId, new List<Action<IModel>> { action });
-                return;
-            }
-
-            OutstandingOperations[transactionId].Add(action);
+                return new List<Action<IModel>> { action };
+            }, (s, list) =>
+            {
+                list.Add(action);
+                return list;
+            });
         }
 
         void ExecuteActionsAgainstRabbitMq(object sender, TransactionEventArgs transactionEventArgs)
         {
             var transactionInfo = transactionEventArgs.Transaction.TransactionInformation;
-
-            if (transactionInfo.Status != TransactionStatus.Committed)
-            {
-                OutstandingOperations.Clear();
-                return;
-            }
-
             var transactionId = transactionInfo.LocalIdentifier;
 
-            if (!OutstandingOperations.ContainsKey(transactionId))
+            if (transactionInfo.Status != TransactionStatus.Committed)
                 return;
 
-            var actions = OutstandingOperations[transactionId];
+            IList<Action<IModel>> actions;
+            OutstandingOperations.TryRemove(transactionId, out actions);
 
             if (!actions.Any())
                 return;
 
             ExecuteRabbitMqActions(actions);
-
-            OutstandingOperations.Clear();
         }
 
         void ExecuteRabbitMqActions(IList<Action<IModel>> actions)
@@ -79,7 +72,6 @@
                 {
                     channel.ConfirmSelect();
                 }
-
 
                 foreach (var action in actions)
                 {
@@ -103,18 +95,6 @@
             }
         }
 
-
-        IDictionary<string, IList<Action<IModel>>> OutstandingOperations
-        {
-            get
-            {
-                return outstandingOperations ?? (outstandingOperations = new Dictionary<string, IList<Action<IModel>>>());
-            }
-        }
-
-
-        //we use a dictionary to make sure that actions from other tx doesn't spill over if threads are getting reused by the hosting infrastructure
-        [ThreadStatic]
-        static IDictionary<string, IList<Action<IModel>>> outstandingOperations;
+        private static ConcurrentDictionary<string, IList<Action<IModel>>> OutstandingOperations = new ConcurrentDictionary<string, IList<Action<IModel>>>();
     }
 }
