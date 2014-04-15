8ac0f98b8c45ca9f31e76dac4b480f1ad2296b4c
John Simons <john.simons@nservicebus.com>
2014-04-15 16:30:13 +1000

    Waiting for all threads to finish current message processing

---
+++ src/NServiceBus.RabbitMQ.Tests/When_stopping_endpoint.cs
@@ -0,0 +1,33 @@
+﻿namespace NServiceBus.Transports.RabbitMQ.Tests
+{
+    using System.Threading;
+    using System.Threading.Tasks;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class When_stopping_endpoint : RabbitMqContext
+    {
+        [SetUp]
+        public new void SetUp()
+        {
+            MakeSureQueueAndExchangeExists(ReceiverQueue);
+        }
+
+        [Test, Explicit]
+        public void Should__gracefully_shutdown()
+        {
+            dequeueStrategy.Stop();
+
+            var address = Address.Parse(ReceiverQueue);
+
+            Parallel.For(0, 2000, i =>
+                sender.Send(new TransportMessage(), address));
+
+            dequeueStrategy.PurgeOnStartup = false;
+            dequeueStrategy.Start(50);
+            Thread.Sleep(10);
+            dequeueStrategy.Stop();
+            connectionManager.Dispose();
+        }
+    }
+}
