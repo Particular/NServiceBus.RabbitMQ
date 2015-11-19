72c1d5bad2ca0fb1804aa8845bec0abb9e8e7aac
SzymonPobiega <szymon.pobiega@gmail.com>
2015-11-19 11:37:01 +0100

    Fixed usage of callback receiver header
     * Moved reading incoming header to behavior
     * Moved adding our own header to behavior
     * Removed forward callback header behavior

---
+++ src/NServiceBus.RabbitMQ/SetOutgoingCallbackAddressBehavior.cs
@@ -0,0 +1,27 @@
+﻿namespace NServiceBus.Transports.RabbitMQ
+{
+    using System;
+    using NServiceBus.Pipeline;
+    using NServiceBus.Pipeline.Contexts;
+
+    class SetOutgoingCallbackAddressBehavior : IBehavior<OutgoingContext>
+    {
+        public string CallbackQueue { get; set; }
+
+        public void Invoke(OutgoingContext context, Action next)
+        {
+            context.OutgoingMessage.Headers[RabbitMqMessageSender.CallbackHeaderKey] = CallbackQueue;
+            next();
+        }
+
+        public class Registration : RegisterStep
+        {
+            public Registration()
+                : base("SetOutgoingCallbackAddressBehavior", typeof(SetOutgoingCallbackAddressBehavior), "Writes out callback address to in outgoing message.")
+            {
+                InsertAfter(WellKnownStep.SerializeMessage);
+                InsertBefore(WellKnownStep.DispatchMessageToTransport);
+            }
+        }
+    }
+}
