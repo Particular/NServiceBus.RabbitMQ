d0604225d7afab6d885d6e6ef9017c9e0d71c0a7
John Simons <john.simons@nservicebus.com>
2015-12-15 16:32:39 +1000

    RabbitMQTransport definition

--- src/NServiceBus.RabbitMQ/Config/RabbitMqTransport.cs
+++ src/NServiceBus.RabbitMQ/Config/RabbitMqTransport.cs
@@ -2,74 +2,137 @@
 {
     using System;
     using System.Collections.Generic;
-    using System.Runtime.InteropServices;
     using System.Text;
-    using Configuration.AdvanceExtensibility;
-    using Features;
-    using NServiceBus.Performance.TimeToBeReceived;
+    using System.Threading.Tasks;
     using NServiceBus.Settings;
+    using NServiceBus.Transports;
     using NServiceBus.Transports.RabbitMQ;
-    using Transports;
+    using NServiceBus.Transports.RabbitMQ.Config;
+    using NServiceBus.Transports.RabbitMQ.Connection;
+    using NServiceBus.Transports.RabbitMQ.Routing;
 
     /// <summary>
-    /// Transport definition for RabbirtMQ
+    ///     Transport definition for RabbirtMQ
     /// </summary>
     public class RabbitMQTransport : TransportDefinition
     {
+        private ConnectionConfiguration connectionConfiguration;
+        private IManageRabbitMqConnections connectionManager;
+        private IRoutingTopology topology;
+
         /// <summary>
-        /// Ctor
+        ///     Ctor
         /// </summary>
         public RabbitMQTransport()
         {
             RequireOutboxConsent = false;
-            //HasNativePubSubSupport = true;
-            //HasSupportForCentralizedPubSub = true;
-            //HasSupportForDistributedTransactions = false;
         }
 
-        ///// <summary>
-        ///// Gives implementations access to the <see cref="T:NServiceBus.BusConfiguration"/> instance at configuration time.
-        ///// </summary>
-        //protected override void Configure(BusConfiguration config)
-        //{
-        //    config.EnableFeature<RabbitMqTransportFeature>();
-        //    config.EnableFeature<TimeoutManagerBasedDeferral>();
-
-        //    config.GetSettings().EnableFeatureByDefault<TimeoutManager>();
-
-        //    //enable the outbox unless the users hasn't disabled it
-        //    if (config.GetSettings().GetOrDefault<bool>(typeof(Features.Outbox).FullName))
-        //    {
-        //        config.EnableOutbox();
-        //    }
-        //}
+        /// <summary>
+        ///     Gets an example connection string to use when reporting lack of configured connection string to the user.
+        /// </summary>
+        public override string ExampleConnectionStringForErrorMessage => "host=localhost";
 
         /// <summary>
-        /// Configures transport for receiving.
+        ///     Configures transport for receiving.
         /// </summary>
         protected override TransportReceivingConfigurationResult ConfigureForReceiving(TransportReceivingConfigurationContext context)
         {
-        return new TransportReceivingConfigurationResult();
+            Initialize(context.Settings, context.ConnectionString);
+
+            return new TransportReceivingConfigurationResult(
+                () => new FakePusher(),
+                () => new RabbitMqQueueCreator(connectionManager, topology, context.Settings),
+                () => Task.FromResult(StartupCheckResult.Success));
         }
 
         /// <summary>
-        /// Configures transport for sending.
+        ///     Configures transport for sending.
         /// </summary>
         protected override TransportSendingConfigurationResult ConfigureForSending(TransportSendingConfigurationContext context)
         {
-            return new TransportSendingConfigurationResult(() => new RabbitMqMessageSender(), );
+            Initialize(context.Settings, context.ConnectionString);
+
+            var provider = new ChannelProvider(connectionManager, connectionConfiguration.UsePublisherConfirms, connectionConfiguration.MaxWaitTimeForConfirms);
+
+            return new TransportSendingConfigurationResult(() => new RabbitMqMessageSender(topology, provider), () => Task.FromResult(StartupCheckResult.Success));
+        }
+
+        private void CreateConnectionManager()
+        {
+            if (connectionManager != null)
+            {
+                return;
+            }
+
+            connectionManager = new RabbitMqConnectionManager(new RabbitMqConnectionFactory(connectionConfiguration), connectionConfiguration);
+        }
+
+        private void Initialize(ReadOnlySettings settings, string connectionString)
+        {
+            CreateTopology(settings);
+            CreateConnectionConfiguration(settings, connectionString);
+            CreateConnectionManager();
+        }
+
+        private void CreateConnectionConfiguration(ReadOnlySettings settings, string connectionString)
+        {
+            if (connectionConfiguration != null)
+            {
+                return;
+            }
+
+            connectionConfiguration = new ConnectionStringParser(settings).Parse(connectionString);
+        }
+
+        private void CreateTopology(ReadOnlySettings settings)
+        {
+            if (topology != null)
+            {
+                return;
+            }
+
+            if (settings.HasSetting<IRoutingTopology>())
+            {
+                topology = settings.Get<IRoutingTopology>();
+            }
+            else
+            {
+                var durable = GetDurableMessagesEnabled(settings);
+
+                DirectRoutingTopology.Conventions conventions;
+
+                if (settings.TryGet(out conventions))
+                {
+                    topology = new DirectRoutingTopology(conventions, durable);
+                }
+                else
+                {
+                    topology = new ConventionalRoutingTopology(durable);
+                }
+            }
+        }
+
+        private static bool GetDurableMessagesEnabled(ReadOnlySettings settings)
+        {
+            bool durableMessagesEnabled;
+            if (settings.TryGet("Endpoint.DurableMessages", out durableMessagesEnabled))
+            {
+                return durableMessagesEnabled;
+            }
+            return true;
         }
 
         /// <summary>
-        /// Returns the list of supported delivery constraints for this transport.
+        ///     Returns the list of supported delivery constraints for this transport.
         /// </summary>
         public override IEnumerable<Type> GetSupportedDeliveryConstraints()
         {
-             ???
+            yield break;
         }
 
         /// <summary>
-        /// Gets the highest supported transaction mode for the this transport.
+        ///     Gets the highest supported transaction mode for the this transport.
         /// </summary>
         public override TransportTransactionMode GetSupportedTransactionMode()
         {
@@ -77,8 +140,8 @@
         }
 
         /// <summary>
-        /// Will be called if the transport has indicated that it has native support for pub sub.
-        ///             Creates a transport address for the input queue defined by a logical address.
+        ///     Will be called if the transport has indicated that it has native support for pub sub.
+        ///     Creates a transport address for the input queue defined by a logical address.
         /// </summary>
         public override IManageSubscriptions GetSubscriptionManager()
         {
@@ -86,7 +149,7 @@
         }
 
         /// <summary>
-        /// Returns the discriminator for this endpoint instance.
+        ///     Returns the discriminator for this endpoint instance.
         /// </summary>
         public override string GetDiscriminatorForThisEndpointInstance(ReadOnlySettings settings)
         {
@@ -94,11 +157,11 @@
         }
 
         /// <summary>
-        /// Converts a given logical address to the transport address.
+        ///     Converts a given logical address to the transport address.
         /// </summary>
         /// <param name="logicalAddress">The logical address.</param>
         /// <returns>
-        /// The transport address.
+        ///     The transport address.
         /// </returns>
         public override string ToTransportAddress(LogicalAddress logicalAddress)
         {
@@ -117,16 +180,38 @@
         }
 
         /// <summary>
-        /// Returns the outbound routing policy selected for the transport.
+        ///     Returns the outbound routing policy selected for the transport.
         /// </summary>
         public override OutboundRoutingPolicy GetOutboundRoutingPolicy(ReadOnlySettings settings)
         {
             return new OutboundRoutingPolicy(OutboundRoutingType.Unicast, OutboundRoutingType.Multicast, OutboundRoutingType.Unicast);
         }
 
-        /// <summary>
-        /// Gets an example connection string to use when reporting lack of configured connection string to the user.
-        /// </summary>
-        public override string ExampleConnectionStringForErrorMessage => "host=localhost";
+        private class FakePusher : IPushMessages
+        {
+            /// <summary>
+            ///     Initializes the <see cref="T:NServiceBus.Transports.IPushMessages" />.
+            /// </summary>
+            public Task Init(Func<PushContext, Task> pipe, CriticalError criticalError, PushSettings settings)
+            {
+                throw new NotImplementedException();
+            }
+
+            /// <summary>
+            ///     Starts pushing message/&gt;.
+            /// </summary>
+            public void Start(PushRuntimeSettings limitations)
+            {
+                throw new NotImplementedException();
+            }
+
+            /// <summary>
+            ///     Stops pushing messages.
+            /// </summary>
+            public Task Stop()
+            {
+                throw new NotImplementedException();
+            }
+        }
     }
 }
