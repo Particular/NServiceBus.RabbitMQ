bd2b7540e7f91caa2e9ccc90ad3f8a7fd2dd1f97
andreasohlund <andreas.ohlund@particular.net>
2015-12-16 11:53:31 +0100

    Moved the callback handling into the pump and dispatcher

--- src/NServiceBus.RabbitMQ/Config/RabbitMqTransport.cs
+++ src/NServiceBus.RabbitMQ/Config/RabbitMqTransport.cs
@@ -104,9 +104,22 @@
         {
             Initialize(context.Settings, context.ConnectionString);
 
+            CallbackAddress callbackAddress;
+
+            if (context.Settings.Get<bool>(UseCallbackReceiverSettingKey))
+            {
+                var queueName = context.Settings.Get<string>("NServiceBus.LocalAddress");
+                var callbackQueue = $"{queueName}.{RuntimeEnvironment.MachineName}";
+                callbackAddress = new CallbackAddress(callbackQueue);
+            }
+            else
+            {
+                callbackAddress = CallbackAddress.None();
+            }
+
             var provider = new ChannelProvider(connectionManager, connectionConfiguration.UsePublisherConfirms, connectionConfiguration.MaxWaitTimeForConfirms);
 
-            return new TransportSendingConfigurationResult(() => new RabbitMqMessageSender(topology, provider), () => Task.FromResult(StartupCheckResult.Success));
+            return new TransportSendingConfigurationResult(() => new RabbitMqMessageSender(topology, provider, callbackAddress), () => Task.FromResult(StartupCheckResult.Success));
         }
 
         private void CreateConnectionManager()
