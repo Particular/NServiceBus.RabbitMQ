bc226313cf0bb772619887db1256cf25c522bef8
Brandon Ording <bording@gmail.com>
2015-12-22 17:46:05 -0500

    Improve perf of Should_gracefully_shutdown()
    - reintroduced Parallel.For
    - removed redundant setup/teardown

--- src/NServiceBus.RabbitMQ.Tests/When_stopping_endpoint.cs
+++ src/NServiceBus.RabbitMQ.Tests/When_stopping_endpoint.cs
@@ -1,40 +1,32 @@
 ﻿namespace NServiceBus.Transports.RabbitMQ.Tests
 {
-    using System.Collections.Generic;
+    using System.Collections.Concurrent;
     using System.Threading.Tasks;
     using NUnit.Framework;
 
     [TestFixture]
     class When_stopping_endpoint : RabbitMqContext
     {
-        [SetUp]
-        public new void SetUp()
-        {
-            MakeSureQueueAndExchangeExists(ReceiverQueue);
-        }
-
         [Test, Explicit]
         public async Task Should_gracefully_shutdown()
         {
             await messagePump.Stop();
 
-            //TODO: maybe build up a IEnumerable<TransportOperation> and Dispatch all at once instead?
-            var tasks = new List<Task>();
+            var tasks = new ConcurrentBag<Task>();
 
-            for (var i = 0; i < 2000; i++)
+            Parallel.For(0, 2000, i =>
             {
                 var operations = new OutgoingMessageBuilder().WithBody(new byte[1]).SendTo(ReceiverQueue).Build();
                 var task = messageSender.Dispatch(operations, new Extensibility.ContextBag());
 
                 tasks.Add(task);
-            }
+            });
 
             await Task.WhenAll(tasks);
 
             messagePump.Start(new PushRuntimeSettings(50));
             await Task.Delay(1000);
             await messagePump.Stop();
-            connectionManager.Dispose();
         }
     }
 }
