e6e7fe2f4cf6f1aeed651a945d794419cccc3e88
John Simons <john.simons@nservicebus.com>
2016-01-08 12:39:31 +1000

    Implementing discriminator logic as per @SzymonPobiega explained

--- src/NServiceBus.RabbitMQ/Config/RabbitMqTransport.cs
+++ src/NServiceBus.RabbitMQ/Config/RabbitMqTransport.cs
@@ -222,6 +222,10 @@
         {
             var queue = new StringBuilder(logicalAddress.EndpointInstance.Endpoint.ToString());
 
+            if (logicalAddress.EndpointInstance.Discriminator != null)
+            {
+                queue.Append("-" + logicalAddress.EndpointInstance.Discriminator);
+            }
             if (logicalAddress.Qualifier != null)
             {
                 queue.Append("." + logicalAddress.Qualifier);
