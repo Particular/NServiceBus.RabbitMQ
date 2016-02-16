04315e84bf1143d6d913bc31498880783953571d
Brandon Ording <bording@gmail.com>
2016-02-16 17:23:48 -0500

    Uncomment callbacks test
    added callbacks package and updated test

--- src/NServiceBus.RabbitMQ.AcceptanceTests/When_scaling_out_senders_that_uses_callbacks.cs
+++ src/NServiceBus.RabbitMQ.AcceptanceTests/When_scaling_out_senders_that_uses_callbacks.cs
@@ -1,115 +1,104 @@
-﻿//namespace NServiceBus.RabbitMQ.AcceptanceTests
-//{
-//    using System;
-//    using NServiceBus.AcceptanceTesting;
-//    using NServiceBus.AcceptanceTesting.Support;
-//    using NServiceBus.AcceptanceTests;
-//    using NServiceBus.AcceptanceTests.EndpointTemplates;
-//    using NServiceBus.Support;
-//    using NUnit.Framework;
+﻿namespace NServiceBus.RabbitMQ.AcceptanceTests
+{
+    using System;
+    using NServiceBus.AcceptanceTesting;
+    using NServiceBus.AcceptanceTests;
+    using NServiceBus.AcceptanceTests.EndpointTemplates;
+    using NUnit.Framework;
+    using System.Threading.Tasks;
 
-//    public class When_scaling_out_senders_that_uses_callbacks : NServiceBusAcceptanceTest
-//    {
-//        const int numMessagesToSend = 5;
+    public class When_scaling_out_senders_that_uses_callbacks : NServiceBusAcceptanceTest
+    {
+        const int numMessagesToSend = 5;
 
-//        [Test]
-//        public void Should_only_deliver_response_to_one_of_the_instances()
-//        {
-//            var context = new Context();
+        [Test]
+        public async Task Should_only_deliver_response_to_one_of_the_instances()
+        {
+            var context = await Scenario.Define<Context>()
+                .WithEndpoint<ServerThatRespondsToCallbacks>(b =>
+                {
+                    b.CustomConfig(c => c.ScaleOut().InstanceDiscriminator("A"));
+                })
+                .WithEndpoint<ScaledOutClient>(b =>
+                {
+                    b.CustomConfig(c => c.ScaleOut().InstanceDiscriminator("A"));
+                    b.When(async (bus, c) =>
+                    {
+                        for (var i = 0; i < numMessagesToSend; i++)
+                        {
+                            var sendOptions = new SendOptions();
+                            var response = await bus.Request<int>(new MyRequest { ReturnCode = 1 }, sendOptions);
 
-//            Scenario.Define(context)
-//                .WithEndpoint<ServerThatRespondsToCallbacks>()
-//                .WithEndpoint<ScaledOutClient>(b =>
-//                {
-//                    b.CustomConfig(c => RuntimeEnvironment.MachineNameAction = () => "ScaledOutClientA");
-//                    b.Given((bus, c) =>
-//                    {
-//                        for (var i = 0; i < numMessagesToSend; i++)
-//                        {
-//                            bus.Send(new MyRequest
-//                            {
-//                                ReturnCode = 1
-//                            })
-//                                .Register<int>(r =>
-//                                {
-//                                    if (r != 1)
-//                                    {
-//                                        throw new Exception("Wrong server got the response");
-//                                    }
-//                                    c.ServerAGotTheCallback++;
-//                                });
-//                        }
-//                    });
-//                })
-//                .WithEndpoint<ScaledOutClient>(b =>
-//                {
-//                    b.CustomConfig(c => RuntimeEnvironment.MachineNameAction = () => "ScaledOutClientB");
-//                    b.Given((bus, c) =>
-//                    {
-//                        for (var i = 0; i < numMessagesToSend; i++)
-//                        {
-//                            bus.Send(new MyRequest
-//                            {
-//                                ReturnCode = 2
-//                            })
-//                                .Register<int>(r =>
-//                                {
-//                                    if (r != 2)
-//                                    {
-//                                        throw new Exception("Wrong server got the response");
-//                                    }
-//                                    c.ServerBGotTheCallback++;
-//                                });
-//                        }
-//                    });
-//                })
-//                .Done(c => (context.ServerAGotTheCallback + context.ServerBGotTheCallback) >= numMessagesToSend*2)
-//                .Run(new RunSettings
-//                {
-//                    UseSeparateAppDomains = true,
-//                });
+                            if (response != 1)
+                            {
+                                throw new Exception("Wrong server got the response");
+                            }
 
-//            Assert.AreEqual(numMessagesToSend, context.ServerAGotTheCallback, "Both scaled out instances should get the callback");
+                            c.ServerAGotTheCallback++;
+                        }
+                    });
+                })
+                .WithEndpoint<ScaledOutClient>(b =>
+                {
+                    b.CustomConfig(c => c.ScaleOut().InstanceDiscriminator("B"));
+                    b.When(async (bus, c) =>
+                    {
+                        for (var i = 0; i < numMessagesToSend; i++)
+                        {
+                            var sendOptions = new SendOptions();
+                            var response = await bus.Request<int>(new MyRequest { ReturnCode = 2 }, sendOptions);
 
-//            Assert.AreEqual(numMessagesToSend, context.ServerBGotTheCallback, "Both scaled out instances should get the callback");
-//        }
+                            if (response != 2)
+                            {
+                                throw new Exception("Wrong server got the response");
+                            }
 
-//        public class ScaledOutClient : EndpointConfigurationBuilder
-//        {
-//            public ScaledOutClient()
-//            {
-//                EndpointSetup<DefaultServer>()
-//                    .AddMapping<MyRequest>(typeof(ServerThatRespondsToCallbacks));
-//            }
-//        }
+                            c.ServerBGotTheCallback++;
+                        }
+                    });
+                })
+                .Done(c => (c.ServerAGotTheCallback + c.ServerBGotTheCallback) >= numMessagesToSend * 2)
+                .Run();
 
-//        public class ServerThatRespondsToCallbacks : EndpointConfigurationBuilder
-//        {
-//            public ServerThatRespondsToCallbacks()
-//            {
-//                EndpointSetup<DefaultServer>();
-//            }
+            Assert.AreEqual(numMessagesToSend, context.ServerAGotTheCallback, "Both scaled out instances should get the callback");
 
-//            class MyEventHandler : IHandleMessages<MyRequest>
-//            {
-//                public IBus Bus { get; set; }
+            Assert.AreEqual(numMessagesToSend, context.ServerBGotTheCallback, "Both scaled out instances should get the callback");
+        }
 
-//                public void Handle(MyRequest message)
-//                {
-//                    Bus.Return(message.ReturnCode);
-//                }
-//            }
-//        }
+        public class ScaledOutClient : EndpointConfigurationBuilder
+        {
+            public ScaledOutClient()
+            {
+                EndpointSetup<DefaultServer>()
+                    .AddMapping<MyRequest>(typeof(ServerThatRespondsToCallbacks));
+            }
+        }
 
-//        class MyRequest : IMessage
-//        {
-//            public int ReturnCode { get; set; }
-//        }
+        public class ServerThatRespondsToCallbacks : EndpointConfigurationBuilder
+        {
+            public ServerThatRespondsToCallbacks()
+            {
+                EndpointSetup<DefaultServer>();
+            }
 
-//        class Context : ScenarioContext
-//        {
-//            public int ServerAGotTheCallback { get; set; }
-//            public int ServerBGotTheCallback { get; set; }
-//        }
-//    }
-//}
+            class MyEventHandler : IHandleMessages<MyRequest>
+            {
+                public async Task Handle(MyRequest message, IMessageHandlerContext context)
+                {
+                    await context.Reply(message.ReturnCode);
+                }
+            }
+        }
+
+        class MyRequest : IMessage
+        {
+            public int ReturnCode { get; set; }
+        }
+
+        class Context : ScenarioContext
+        {
+            public int ServerAGotTheCallback { get; set; }
+            public int ServerBGotTheCallback { get; set; }
+        }
+    }
+}
