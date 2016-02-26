58473e354552538aab977275d48596efe26eec99
Brandon Ording <bording@gmail.com>
2016-02-25 19:33:33 -0500

    Fix redundant using directive code inspection error

--- src/NServiceBus.RabbitMQ/Config/ConnectionConfiguration.cs
+++ src/NServiceBus.RabbitMQ/Config/ConnectionConfiguration.cs
@@ -4,7 +4,6 @@
     using System.Collections.Generic;
     using System.Diagnostics;
     using System.IO;
-    using System.Reflection;
     using NServiceBus.Settings;
     using NServiceBus.Support;
 
