b85565e4c629b618bddb0151aac98c46c189566e
Simon Cropp <simon.cropp@gmail.com>
2016-04-07 19:06:27 +1000

    update to beta1

---
+++ src/NServiceBus.RabbitMQ.AcceptanceTests/App_Packages/NSB.AcceptanceTests.6.0.0-beta0001/Recoverability/Retries/When_fails_with_retries_set_to_0.cs
@@ -0,0 +1,74 @@
+﻿namespace NServiceBus.AcceptanceTests.Recoverability.Retries
+{
+    using System;
+    using System.Threading.Tasks;
+    using AcceptanceTesting;
+    using EndpointTemplates;
+    using Features;
+    using NServiceBus.Config;
+    using NUnit.Framework;
+
+    public class When_fails_with_retries_set_to_0 : NServiceBusAcceptanceTest
+    {
+        [Test]
+        public async Task Should_not_retry_the_message_using_flr()
+        {
+            var context = await Scenario.Define<Context>(c => { c.Id = Guid.NewGuid(); })
+                .WithEndpoint<RetryEndpoint>(b =>
+                {
+                    b.DoNotFailOnErrorMessages();
+                    b.When((session, c) => session.SendLocal(new MessageToBeRetried
+                    {
+                        ContextId = c.Id
+                    }));
+                })
+                .Done(c => c.GaveUp)
+                .Run();
+
+            Assert.AreEqual(1, context.NumberOfTimesInvoked, "No FLR should be in use if MaxRetries is set to 0");
+        }
+
+        class Context : ScenarioContext
+        {
+            public Guid Id { get; set; }
+            public int NumberOfTimesInvoked { get; set; }
+
+            public bool GaveUp { get; set; }
+        }
+
+        public class RetryEndpoint : EndpointConfigurationBuilder
+        {
+            public RetryEndpoint()
+            {
+                EndpointSetup<DefaultServer>((configure, context) =>
+                {
+                    var scenarioContext = (Context) context.ScenarioContext;
+                    configure.EnableFeature<FirstLevelRetries>();
+                    configure.Notifications.Errors.MessageSentToErrorQueue += (sender, message) => scenarioContext.GaveUp = true;
+                })
+                    .WithConfig<TransportConfig>(c => { c.MaxRetries = 0; });
+            }
+
+            class MessageToBeRetriedHandler : IHandleMessages<MessageToBeRetried>
+            {
+                public Context Context { get; set; }
+
+                public Task Handle(MessageToBeRetried message, IMessageHandlerContext context)
+                {
+                    if (Context.Id != message.ContextId)
+                    {
+                        return Task.FromResult(0);
+                    }
+                    Context.NumberOfTimesInvoked++;
+                    throw new SimulatedException();
+                }
+            }
+        }
+
+        [Serializable]
+        public class MessageToBeRetried : IMessage
+        {
+            public Guid ContextId { get; set; }
+        }
+    }
+}
