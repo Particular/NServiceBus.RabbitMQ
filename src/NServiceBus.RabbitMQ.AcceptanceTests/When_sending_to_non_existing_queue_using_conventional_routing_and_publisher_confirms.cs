70dfdd05aaaa9072549ef3066d44e6d448a6285b
Michał Wójcik <wojcik.mike@gmail.com>
2016-09-06 14:26:40 +0200

    A set of test to verify the behavior when sending to non existing queue using publisher confirms true/false and conventional/direct routing topology

---
+++ src/NServiceBus.RabbitMQ.AcceptanceTests/When_sending_to_non_existing_queue_using_conventional_routing_and_publisher_confirms.cs
@@ -0,0 +1,55 @@
+﻿namespace NServiceBus.Transport.RabbitMQ.AcceptanceTests
+{
+    using System;
+    using System.Threading.Tasks;
+    using AcceptanceTesting;
+    using NServiceBus.AcceptanceTests;
+    using NServiceBus.AcceptanceTests.EndpointTemplates;
+    using NUnit.Framework;
+
+    public class When_sending_to_non_existing_queue_using_conventional_routing_and_publisher_confirms : NServiceBusAcceptanceTest
+    {
+        [Test]
+        public async Task Should_throw_exception()
+        {
+            var context = await Scenario.Define<MyContext>()
+                .WithEndpoint<Sender>(b => b.When(async (bus, c) =>
+                {
+                    var sendOptions = new SendOptions();
+                    sendOptions.SetDestination("nonExistingQueue");
+                    try
+                    {
+                        await bus.Send(new MyRequest(), sendOptions);
+                    }
+                    catch (Exception ex)
+                    {
+                        c.Exception = ex;
+                        c.GotTheException = true;
+                    }
+                }))
+                .Done(c => c.GotTheException)
+                .Run();
+            
+            Assert.IsInstanceOf<Exception>(context.Exception);
+        }
+
+        public class Sender : EndpointConfigurationBuilder
+        {
+            public Sender()
+            {
+                EndpointSetup<DefaultServer>(c => c.UseTransport<RabbitMQTransport>()
+                    .UsePublisherConfirms(true));
+            }
+        }
+
+        class MyRequest : IMessage
+        {
+        }
+
+        class MyContext : ScenarioContext
+        {
+            public bool GotTheException { get; set; }
+            public Exception Exception { get; set; }
+        }
+    }
+}
---
+++ src/NServiceBus.RabbitMQ.AcceptanceTests/When_sending_to_non_existing_queue_using_direct_routing_without_publisher_confirms.cs
@@ -0,0 +1,42 @@
+﻿namespace NServiceBus.Transport.RabbitMQ.AcceptanceTests
+{
+    using System;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using AcceptanceTesting;
+    using NServiceBus.AcceptanceTests;
+    using NServiceBus.AcceptanceTests.EndpointTemplates;
+    using NUnit.Framework;
+
+    public class When_sending_to_non_existing_queue_using_direct_routing_without_publisher_confirms : NServiceBusAcceptanceTest
+    {
+        [Test]
+        public async Task Should_log_entry()
+        {
+            var context = await Scenario.Define<ScenarioContext>()
+                .WithEndpoint<Sender>(b => b.When((bus, c) =>
+                {
+                    var sendOptions = new SendOptions();
+                    sendOptions.SetDestination("nonExistingQueue");
+                    return bus.Send(new MyRequest(), sendOptions);
+
+                }))
+                .Run(TimeSpan.FromSeconds(20));
+
+            Assert.IsTrue(context.Logs.Any(x => x.Message.StartsWith("Message could not be routed to")));
+        }
+
+        public class Sender : EndpointConfigurationBuilder
+        {
+            public Sender()
+            {
+                EndpointSetup<DefaultServer>(c => c.UseTransport<RabbitMQTransport>()
+                    .UseDirectRoutingTopology().UsePublisherConfirms(false));
+            }
+        }
+
+        class MyRequest : IMessage
+        {
+        }
+    }
+}
