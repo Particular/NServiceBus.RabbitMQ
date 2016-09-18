5c255dba7ff1a96d2d73e98b4487f6572489d5a7
Adam Ralph <adam@adamralph.com>
2016-09-18 02:12:57 +0200

    wip

--- build.cmd
+++ build.cmd
@@ -6,13 +6,13 @@ cd %~dp0
 setlocal
 
 :: determine cache dir
-set NUGET_CACHE_DIR=%LocalAppData%\NuGet
+set NUGET_CACHE_DIR=%LocalAppData%\.nuget\v3.4.4
 
 :: download nuget to cache dir
-set NUGET_URL="https://dist.nuget.org/win-x86-commandline/latest/nuget.exe"
+set NUGET_URL=https://dist.nuget.org/win-x86-commandline/v3.4.4/NuGet.exe
 if not exist %NUGET_CACHE_DIR%\NuGet.exe (
   if not exist %NUGET_CACHE_DIR% md %NUGET_CACHE_DIR%
-  echo Downloading latest version of NuGet.exe...
+  echo Downloading '%NUGET_URL%'' to '%NUGET_CACHE_DIR%\NuGet.exe'...
   @powershell -NoProfile -ExecutionPolicy unrestricted -Command "$ProgressPreference = 'SilentlyContinue'; Invoke-WebRequest '%NUGET_URL%' -OutFile '%NUGET_CACHE_DIR%\NuGet.exe'"
 )
 
