97865ce566ebe1ca7f87853e108abded0cd7c33b
Adam Ralph <adam@adamralph.com>
2016-12-14 09:48:43 +0100

    change acceptance tests to UseBackgroundThreadsForIO

--- src/NServiceBus.RabbitMQ.AcceptanceTests/ConfigureEndpointRabbitMQTransport.cs
+++ src/NServiceBus.RabbitMQ.AcceptanceTests/ConfigureEndpointRabbitMQTransport.cs
@@ -52,7 +52,8 @@ class ConfigureEndpointRabbitMQTransport : IConfigureEndpointTestExecution
 
         var connectionFactory = new ConnectionFactory
         {
-            AutomaticRecoveryEnabled = true
+            AutomaticRecoveryEnabled = true,
+            UseBackgroundThreadsForIO = true
         };
 
         object value;
