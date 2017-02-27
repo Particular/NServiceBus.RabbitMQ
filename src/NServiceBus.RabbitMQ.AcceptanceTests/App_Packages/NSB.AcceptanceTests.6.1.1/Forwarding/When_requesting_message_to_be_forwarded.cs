3c8778a6b3d15ce983b0ada8783bf5dcf941a773
Brandon Ording <bording@gmail.com>
2017-02-27 16:16:19 -0500

    Undo the test hack that is no longer needed

--- src/NServiceBus.RabbitMQ.AcceptanceTests/App_Packages/NSB.AcceptanceTests.6.1.1/Forwarding/When_requesting_message_to_be_forwarded.cs
+++ src/NServiceBus.RabbitMQ.AcceptanceTests/App_Packages/NSB.AcceptanceTests.6.1.1/Forwarding/When_requesting_message_to_be_forwarded.cs
@@ -1,7 +1,6 @@
 ﻿namespace NServiceBus.AcceptanceTests.Forwarding
 {
     using System.Collections.Generic;
-    using System.Linq;
     using System.Threading.Tasks;
     using AcceptanceTesting;
     using EndpointTemplates;
@@ -19,10 +18,7 @@
                 .Run();
 
             Assert.IsTrue(context.GotForwardedMessage);
-            CollectionAssert.AreEqual(
-                context.ForwardedHeaders.Where(header => header.Key != "NServiceBus.Transport.RabbitMQ.ConfirmationId"),
-                context.ReceivedHeaders.Where(header => header.Key != "NServiceBus.Transport.RabbitMQ.ConfirmationId"),
-                "Headers should be preserved on the forwarded message");
+            CollectionAssert.AreEqual(context.ForwardedHeaders, context.ReceivedHeaders, "Headers should be preserved on the forwarded message");
         }
 
         public class Context : ScenarioContext
