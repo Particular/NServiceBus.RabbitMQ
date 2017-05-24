237cb14454cf072c6a8d535991dea529b403d189
Adam Ralph <adam@adamralph.com>
2017-05-24 16:07:22 +0100

    green: move GetBasicDeliverEventArgs to new extension class

---
+++ src/NServiceBus.Transport.RabbitMQ/Receiving/MessageHandlerContextExtensions.cs
@@ -0,0 +1,16 @@
+﻿namespace NServiceBus
+{
+    using RabbitMQ.Client.Events;
+
+    /// <summary>
+    /// Extensions to <see cref="IMessageHandlerContext"/>.
+    /// </summary>
+    public static class MessageHandlerContextExtensions
+    {
+        /// <summary>
+        /// Gets the <see cref="BasicDeliverEventArgs"/> for the current message being processed.
+        /// </summary>
+        public static BasicDeliverEventArgs GetBasicDeliverEventArgs(this IMessageHandlerContext context) =>
+            context.Extensions.Get<BasicDeliverEventArgs>();
+    }
+}
