edd3278ff6927d558e85a0fd86458c9521a02ef8
Brandon Ording <bording@gmail.com>
2017-08-11 12:46:56 -0400

    Remove obsolete app settings check

--- src/NServiceBus.Transport.RabbitMQ/RabbitMQTransportInfrastructure.cs
+++ src/NServiceBus.Transport.RabbitMQ/RabbitMQTransportInfrastructure.cs
@@ -88,7 +88,7 @@
             return new TransportReceiveInfrastructure(
                     () => CreateMessagePump(),
                     () => new QueueCreator(connectionFactory, routingTopology),
-                    () => Task.FromResult(ObsoleteAppSettings.Check()));
+                    () => Task.FromResult(StartupCheckResult.Success));
         }
 
         public override TransportSendInfrastructure ConfigureSendInfrastructure()
