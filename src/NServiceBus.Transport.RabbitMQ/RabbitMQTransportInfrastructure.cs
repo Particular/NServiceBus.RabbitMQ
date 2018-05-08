bd433aebb8a6cd8482394b1baffe531669626e25
Brandon Ording <bording@gmail.com>
2018-05-08 11:36:37 -0400

    Fix missing using statement from merge

--- src/NServiceBus.Transport.RabbitMQ/RabbitMQTransportInfrastructure.cs
+++ src/NServiceBus.Transport.RabbitMQ/RabbitMQTransportInfrastructure.cs
@@ -6,6 +6,7 @@
     using System.Text;
     using System.Threading.Tasks;
     using DelayedDelivery;
+    using Features;
     using global::RabbitMQ.Client.Events;
     using Performance.TimeToBeReceived;
     using Routing;
