9d5c84f471c26b0c7b2a4b8bf52b25262819444b
Brandon Ording <bording@gmail.com>
2018-12-13 16:44:00 -0500

    Move to Particular.Approvals

--- src/NServiceBus.Transport.RabbitMQ.Tests/APIApprovals.cs
+++ src/NServiceBus.Transport.RabbitMQ.Tests/APIApprovals.cs
@@ -1,30 +1,18 @@
 ﻿namespace NServiceBus.Transport.RabbitMQ.Tests
 {
-    using System;
-    using System.Linq;
-    using System.Runtime.CompilerServices;
     using NServiceBus;
     using NUnit.Framework;
+    using Particular.Approvals;
     using PublicApiGenerator;
 
     [TestFixture]
     public class APIApprovals
     {
         [Test]
-        [MethodImpl(MethodImplOptions.NoInlining)]
         public void Approve()
         {
-            var publicApi = Filter(ApiGenerator.GeneratePublicApi(typeof(RabbitMQTransport).Assembly));
-            TestApprover.Verify(publicApi);
-        }
-
-        string Filter(string api)
-        {
-            var lines = api.Split(new[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries)
-                .Where(line => !line.StartsWith("[assembly: System.Runtime.Versioning.TargetFrameworkAttribute"))
-                .Where(line => !string.IsNullOrWhiteSpace(line));
-            
-            return string.Join(Environment.NewLine, lines);
+            var publicApi = ApiGenerator.GeneratePublicApi(typeof(RabbitMQTransport).Assembly, excludeAttributes: new[] { "System.Runtime.Versioning.TargetFrameworkAttribute" });
+            Approver.Verify(publicApi);
         }
     }
 }
