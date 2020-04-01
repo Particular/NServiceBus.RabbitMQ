a46e4705b6079b8272286f7f7ca8632e43d90415
Helen T <helenktsai@gmail.com>
2020-04-01 15:25:30 -0700

    Update test associated with update to DefaultRequestedHeartbeat

--- src/NServiceBus.Transport.RabbitMQ.Tests/ConnectionString/ConnectionConfigurationTests.cs
+++ src/NServiceBus.Transport.RabbitMQ.Tests/ConnectionString/ConnectionConfigurationTests.cs
@@ -210,7 +210,7 @@
         [Test]
         public void Should_set_default_requested_heartbeat()
         {
-            Assert.AreEqual(defaults.RequestedHeartbeat, 5);
+            Assert.AreEqual(defaults.RequestedHeartbeat, 60);
         }
 
         [Test]
