663c1f1b594304041f44c9523e37edb6da4ef02c
Sean Feldman <feldman.sean@gmail.com>
2020-04-10 14:08:50 -0600

    Obsolete old configuration for certificates collection with a single certificate

--- src/NServiceBus.Transport.RabbitMQ/Configuration/RabbitMQTransportSettingsExtensions.cs
+++ src/NServiceBus.Transport.RabbitMQ/Configuration/RabbitMQTransportSettingsExtensions.cs
@@ -157,12 +157,29 @@
         /// <param name="transportExtensions"></param>
         /// <param name="clientCertificates">The collection of certificates to use for client authentication.</param>
         /// <returns></returns>
+        [ObsoleteEx(RemoveInVersion = "7.0", TreatAsErrorFromVersion = "6.0", Message = "", ReplacementTypeOrMember = "SetClientCertificate(X509Certificate2 clientCertificate)")]
         public static TransportExtensions<RabbitMQTransport> SetClientCertificates(this TransportExtensions<RabbitMQTransport> transportExtensions, X509CertificateCollection clientCertificates)
         {
             Guard.AgainstNull(nameof(transportExtensions), transportExtensions);
             Guard.AgainstNull(nameof(clientCertificates), clientCertificates);
 
             transportExtensions.GetSettings().Set(SettingsKeys.ClientCertificates, clientCertificates);
+
+            return transportExtensions;
+        }
+
+        /// <summary>
+        /// Specifies the certificate to use for client authentication when connecting to the broker via TLS.
+        /// </summary>
+        /// <param name="transportExtensions"></param>
+        /// <param name="clientCertificate">The collection of certificates to use for client authentication.</param>
+        /// <returns></returns>
+        public static TransportExtensions<RabbitMQTransport> SetClientCertificate(this TransportExtensions<RabbitMQTransport> transportExtensions, X509Certificate2 clientCertificate)
+        {
+            Guard.AgainstNull(nameof(transportExtensions), transportExtensions);
+            Guard.AgainstNull(nameof(clientCertificate), clientCertificate);
+
+            transportExtensions.GetSettings().Set(SettingsKeys.ClientCertificates, new X509Certificate2Collection(clientCertificate));
 
             return transportExtensions;
         }
