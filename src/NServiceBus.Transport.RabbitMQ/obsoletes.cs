bd914cc4edd520d8946feaf938fa505eb4eab95d
Brandon Ording <bording@gmail.com>
2020-05-27 16:41:36 -0400

    Update obsoletes

--- src/NServiceBus.Transport.RabbitMQ/obsoletes.cs
+++ src/NServiceBus.Transport.RabbitMQ/obsoletes.cs
@@ -3,26 +3,19 @@
 namespace NServiceBus
 {
     using System;
+    using System.Security.Cryptography.X509Certificates;
     using Transport.RabbitMQ;
 
-    public static partial class RabbitMQTransportSettingsExtensions
+    public partial class RabbitMQTransportSettingsExtensions
     {
-        [ObsoleteEx(RemoveInVersion = "6.0", TreatAsErrorFromVersion = "5.0", ReplacementTypeOrMember = "RabbitMQTransportSettingsExtensions.UseCustomRoutingTopology(TransportExtensions<RabbitMQTransport> transportExtensions, Func<bool, IRoutingTopology>)")]
-        public static TransportExtensions<RabbitMQTransport> UseRoutingTopology<T>(this TransportExtensions<RabbitMQTransport> transportExtensions) where T : IRoutingTopology, new()
+        [ObsoleteEx(RemoveInVersion = "7.0", TreatAsErrorFromVersion = "6.0", ReplacementTypeOrMember = "RabbitMQTransportSettingsExtensions.UseCustomRoutingTopology(TransportExtensions<RabbitMQTransport> transportExtensions, Func<bool, IRoutingTopology>)")]
+        public static TransportExtensions<RabbitMQTransport> UseRoutingTopology(this TransportExtensions<RabbitMQTransport> transportExtensions, Func<bool, IRoutingTopology> topologyFactory)
         {
             throw new NotImplementedException();
         }
-    }
-}
 
-namespace NServiceBus.Transport.RabbitMQ
-{
-    using System;
-
-    public partial class DelayedDeliverySettings
-    {
-        [ObsoleteEx(RemoveInVersion = "6.0", TreatAsErrorFromVersion = "5.0", Message = "The timeout manager is now disabled by default.")]
-        public DelayedDeliverySettings DisableTimeoutManager()
+        [ObsoleteEx(RemoveInVersion = "7.0", TreatAsErrorFromVersion = "6.0", ReplacementTypeOrMember = "RabbitMQTransportSettingsExtensions.SetClientCertificate(TransportExtensions<RabbitMQTransport> transportExtensions, X509Certificate2 clientCertificate)")]
+        public static TransportExtensions<RabbitMQTransport> SetClientCertificates(this TransportExtensions<RabbitMQTransport> transportExtensions, X509CertificateCollection clientCertificates)
         {
             throw new NotImplementedException();
         }
