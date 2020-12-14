9cb1f70e4605b7a482522e339753f92f61b2652f
Sean Feldman <feldman.sean@gmail.com>
2020-12-14 15:05:47 -0700

    Fix API Approvals

--- src/NServiceBus.Transport.RabbitMQ.Tests/APIApprovals.cs
+++ src/NServiceBus.Transport.RabbitMQ.Tests/APIApprovals.cs
@@ -11,7 +11,10 @@
         [Test]
         public void Approve()
         {
-            var publicApi = ApiGenerator.GeneratePublicApi(typeof(RabbitMQTransport).Assembly, excludeAttributes: new[] { "System.Runtime.Versioning.TargetFrameworkAttribute" });
+            var publicApi = typeof(RabbitMQTransport).Assembly.GeneratePublicApi(new ApiGeneratorOptions
+            {
+                ExcludeAttributes = new[] { "System.Runtime.Versioning.TargetFrameworkAttribute", "System.Reflection.AssemblyMetadataAttribute" }
+            });
             Approver.Verify(publicApi);
         }
     }
