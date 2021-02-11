278d119d0fabf730b4594886e676678832ae948b
Szymon Pobiega <szymon.pobiega@gmail.com>
2021-02-11 13:11:55 +0100

    Fix path-based tests on linux

--- src/NServiceBus.Transport.RabbitMQ.Tests/ConnectionString/ConnectionConfigurationTests.cs
+++ src/NServiceBus.Transport.RabbitMQ.Tests/ConnectionString/ConnectionConfigurationTests.cs
@@ -1,15 +1,15 @@
 ﻿namespace NServiceBus.Transport.RabbitMQ.Tests.ConnectionString
 {
     using System;
+    using System.IO;
     using NUnit.Framework;
-    using RabbitMQ;
 
     [TestFixture]
     public class ConnectionConfigurationTests
     {
-        const string connectionString =
+        static readonly string connectionString =
             "virtualHost=Copa;username=Copa;host=192.168.1.1:1234;password=abc_xyz;port=12345;requestedHeartbeat=3;" +
-            "retryDelay=01:02:03;useTls=true;certPath=..\\..\\..\\myp12.p12;certPassPhrase=abc123";
+            $"retryDelay=01:02:03;useTls=true;certPath=..{Path.PathSeparator}..{Path.PathSeparator}..{Path.PathSeparator}myp12.p12;certPassPhrase=abc123";
 
         RabbitMQTransport CreateTransportDefinition(string connectionString)
         {
@@ -129,7 +129,7 @@
         [Test]
         public void Should_parse_the_cert_path()
         {
-            var connectionConfiguration = CreateTransportDefinition("host=localhost;certPath=..\\..\\..\\myp12.p12;certPassphrase=abc123");
+            var connectionConfiguration = CreateTransportDefinition($"host=localhost;certPath=...{Path.PathSeparator}...{Path.PathSeparator}...{Path.PathSeparator}myp12.p12;certPassphrase=abc123");
 
             Assert.AreEqual("O=Particular, S=Some-State, C=PL", connectionConfiguration.ClientCertificate.Issuer);
         }
