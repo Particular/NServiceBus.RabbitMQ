710577e6a6de6ca1822d29547398a00ceced872e
Tim Bussmann <timbussmann@users.noreply.github.com>
2021-04-15 14:29:17 +0200

    create acceptance test to test delayed delivery usage

---
+++ src/NServiceBus.Transport.RabbitMQ.AcceptanceTests/When_using_delayed_delivery_with_quorum_queues.cs
@@ -0,0 +1,47 @@
+﻿namespace NServiceBus.Transport.RabbitMQ.AcceptanceTests
+{
+    using System;
+    using AcceptanceTesting;
+    using NServiceBus.AcceptanceTests;
+    using NServiceBus.AcceptanceTests.EndpointTemplates;
+    using NUnit.Framework;
+
+    public class When_using_delayed_delivery_with_quorum_queues : NServiceBusAcceptanceTest
+    {
+        [Test]
+        public void Should_not_allow_delayed_retries()
+        {
+            var exception = Assert.ThrowsAsync<Exception>(async () => await Scenario.Define<ScenarioContext>()
+                .WithEndpoint<EndpointWithQuorumQueue>(b => b
+                    .CustomConfig(config => config
+                        .Recoverability().Delayed(d => d.NumberOfRetries(3))))
+                .Done(c => c.EndpointsStarted)
+                .Run());
+
+            StringAssert.Contains("Delayed retries are not supported when the transport does not support delayed delivery.", exception.Message);
+        }
+
+        //TODO: when using delayed sends
+        //TODO: when using saga timeouts
+
+        public class EndpointWithQuorumQueue : EndpointConfigurationBuilder
+        {
+            public EndpointWithQuorumQueue()
+            {
+                var transportConfiguration = new ConfigureEndpointRabbitMQTransport(useQuorumQueue: true);
+                var defaultServer = new DefaultServer
+                {
+                    TransportConfiguration = transportConfiguration
+                };
+                EndpointSetup(
+                    defaultServer,
+                    (configuration, r) =>
+                    {
+                        // need to configure a different error queue that isn't a classic queue.
+                        configuration.SendFailedMessagesTo("error-quorum");
+                    },
+                    _ => { });
+            }
+        }
+    }
+}
