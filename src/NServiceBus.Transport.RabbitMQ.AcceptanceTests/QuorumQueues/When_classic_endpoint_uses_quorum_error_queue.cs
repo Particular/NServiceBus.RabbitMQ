30fd08dd1ff991c5e3d8e792582e6c8206c096b8
Tim Bussmann <timbussmann@users.noreply.github.com>
2021-04-16 16:05:59 +0200

    add failing tests for quorum compatibility for "classic" endpoints

---
+++ src/NServiceBus.Transport.RabbitMQ.AcceptanceTests/QuorumQueues/When_classic_endpoint_uses_quorum_error_queue.cs
@@ -0,0 +1,48 @@
+﻿namespace NServiceBus.Transport.RabbitMQ.AcceptanceTests
+{
+    using System.Threading.Tasks;
+    using AcceptanceTesting;
+    using NServiceBus.AcceptanceTests;
+    using NServiceBus.AcceptanceTests.EndpointTemplates;
+    using NUnit.Framework;
+
+    public class When_classic_endpoint_uses_quorum_error_queue : NServiceBusAcceptanceTest
+    {
+        [Test]
+        public async Task Should_start_without_error()
+        {
+            await Scenario.Define<ScenarioContext>()
+                .WithEndpoint<QuorumQueueEndpoint>()
+                .Done(c => c.EndpointsStarted)
+                .Run();
+
+            var context = await Scenario.Define<ScenarioContext>()
+                .WithEndpoint<ClassicQueueEndpoint>()
+                .Done(c => c.EndpointsStarted)
+                .Run();
+
+            Assert.IsTrue(context.EndpointsStarted);
+        }
+
+        class ClassicQueueEndpoint : EndpointConfigurationBuilder
+        {
+            public ClassicQueueEndpoint()
+            {
+                EndpointSetup<DefaultServer>(c => c
+                    .SendFailedMessagesTo("rabbitmq.transport.tests.quorum-error"));
+            }
+        }
+
+        class QuorumQueueEndpoint : EndpointConfigurationBuilder
+        {
+            public QuorumQueueEndpoint()
+            {
+                var clusterTemplate = new ClusterEndpoint(QueueMode.Quorum);
+                EndpointSetup(clusterTemplate, (c, __) =>
+                {
+                    c.SendFailedMessagesTo("rabbitmq.transport.tests.quorum-error");
+                }, _ => { });
+            }
+        }
+    }
+}
