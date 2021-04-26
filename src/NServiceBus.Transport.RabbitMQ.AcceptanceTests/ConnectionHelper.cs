562c11af720888ae5f190640b260d8419597c0ef
Tim Bussmann <timbussmann@users.noreply.github.com>
2021-04-26 11:15:08 +0200

    acceptance tests

---
+++ src/NServiceBus.Transport.RabbitMQ.AcceptanceTests/ConnectionHelper.cs
@@ -0,0 +1,62 @@
+﻿namespace NServiceBus.Transport.RabbitMQ.AcceptanceTests
+{
+    using System;
+    using System.Data.Common;
+    using global::RabbitMQ.Client;
+
+    public class ConnectionHelper
+    {
+        static Lazy<string> connectionString = new Lazy<string>(() =>
+        {
+            var connectionString = Environment.GetEnvironmentVariable("RabbitMQTransport_ConnectionString");
+
+            if (string.IsNullOrEmpty(connectionString))
+            {
+                throw new Exception("The 'RabbitMQTransport_ConnectionString' environment variable is not set.");
+            }
+
+            return connectionString;
+        });
+
+        static Lazy<ConnectionFactory> connectionFactory = new Lazy<ConnectionFactory>(() =>
+        {
+            var connectionStringBuilder = new DbConnectionStringBuilder { ConnectionString = ConnectionString };
+
+            var connectionFactory = new ConnectionFactory
+            {
+                AutomaticRecoveryEnabled = true,
+                UseBackgroundThreadsForIO = true
+            };
+
+            if (connectionStringBuilder.TryGetValue("username", out var value))
+            {
+                connectionFactory.UserName = value.ToString();
+            }
+
+            if (connectionStringBuilder.TryGetValue("password", out value))
+            {
+                connectionFactory.Password = value.ToString();
+            }
+
+            if (connectionStringBuilder.TryGetValue("virtualhost", out value))
+            {
+                connectionFactory.VirtualHost = value.ToString();
+            }
+
+            if (connectionStringBuilder.TryGetValue("host", out value))
+            {
+                connectionFactory.HostName = value.ToString();
+            }
+            else
+            {
+                throw new Exception("The connection string doesn't contain a value for 'host'.");
+            }
+
+            return connectionFactory;
+        });
+
+        public static string ConnectionString => connectionString.Value;
+
+        public static ConnectionFactory ConnectionFactory => connectionFactory.Value;
+    }
+}
---
+++ src/NServiceBus.Transport.RabbitMQ.AcceptanceTests/QuorumQueues/When_endpoint_uses_quorum_error_queue.cs
@@ -0,0 +1,45 @@
+﻿namespace NServiceBus.Transport.RabbitMQ.AcceptanceTests
+{
+    using System.Threading.Tasks;
+    using AcceptanceTesting;
+    using NServiceBus.AcceptanceTests;
+    using NServiceBus.AcceptanceTests.EndpointTemplates;
+    using NUnit.Framework;
+
+    public class When_endpoint_uses_quorum_error_queue : NServiceBusAcceptanceTest
+    {
+        const string ErrorQueueName = "rabbitmq.transport.tests.quorum-error";
+
+        [Test]
+        public async Task Should_start_without_error()
+        {
+            using (var connection = ConnectionHelper.ConnectionFactory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.DeclareQuorumQueue(ErrorQueueName);
+            }
+
+            var context = await Scenario.Define<ScenarioContext>()
+                .WithEndpoint<ClassicQueueEndpoint>()
+                .Done(c => c.EndpointsStarted)
+                .Run();
+
+            Assert.IsTrue(context.EndpointsStarted);
+            // Verify error queue is indeed a quorum queue:
+            using (var connection = ConnectionHelper.ConnectionFactory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.DeclareQuorumQueue(ErrorQueueName);
+            }
+        }
+
+        class ClassicQueueEndpoint : EndpointConfigurationBuilder
+        {
+            public ClassicQueueEndpoint()
+            {
+                EndpointSetup<DefaultServer>(c => c
+                    .SendFailedMessagesTo(ErrorQueueName));
+            }
+        }
+    }
+}
