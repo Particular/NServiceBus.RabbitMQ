e960db951f23a895a0c6dabb097aeca8729f339c
Tim Bussmann <timbussmann@users.noreply.github.com>
2021-04-28 14:06:43 +0200

    remove unnecessary namespace import

--- src/NServiceBus.Transport.RabbitMQ/Receiving/MessagePump.cs
+++ src/NServiceBus.Transport.RabbitMQ/Receiving/MessagePump.cs
@@ -2,7 +2,6 @@
 {
     using System;
     using System.Collections.Generic;
-    using System.Globalization;
     using System.Threading;
     using System.Threading.Tasks;
     using Extensibility;
