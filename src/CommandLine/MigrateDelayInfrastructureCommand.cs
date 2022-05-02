209db3a51d54904f6df98d0ba9d83df20028f718
kentdr <dan.kent@particular.net>
2022-05-02 10:14:12 -0400

    Create command line tool

---
+++ src/CommandLine/MigrateDelayInfrastructureCommand.cs
@@ -0,0 +1,39 @@
+﻿namespace NServiceBus.RabbitMQ.CommandLine
+{
+    using System;
+    using System.CommandLine;
+    using System.Threading.Tasks;
+
+    public class MigrateDelayInfrastructureCommand
+    {
+        const string ConnectionStringEnvironmentVariable = "RabbitMQTransport_ConnectionString";
+
+        public static Command CreateCommand()
+        {
+            var connectionStringOption = new Option<string>(
+                    name: "--connectionString",
+                    description: $"Overrides environment variable '{ConnectionStringEnvironmentVariable}'",
+                    getDefaultValue: () => Environment.GetEnvironmentVariable(ConnectionStringEnvironmentVariable) ?? string.Empty);
+
+            connectionStringOption.AddAlias("-c");
+
+            var migrateCommand = new Command("migrate-delay-infrastructure", "Migrate existing delay queues and in-flight delayed messages to the latest infrustructure.");
+            migrateCommand.AddOption(connectionStringOption);
+
+            migrateCommand.SetHandler(async (string connectionString, CancellationToken cancellationToken) =>
+            {
+                var migrationProcess = new MigrateDelayInfrastructureCommand();
+                await migrationProcess.Execute(connectionString, cancellationToken).ConfigureAwait(false);
+
+            }, connectionStringOption);
+
+            return migrateCommand;
+        }
+
+        public Task Execute(string connectionString, CancellationToken cancellationToken = default)
+        {
+            Console.WriteLine("Migrating delay infrustructure...");
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ src/CommandLine/Program.cs
@@ -0,0 +1,8 @@
+﻿using System.CommandLine;
+using NServiceBus.RabbitMQ.CommandLine;
+
+var rootCommand = new RootCommand("A .NET global tool to manage the RabbitMQ transport for NServiceBus endpoints");
+
+rootCommand.AddCommand(MigrateDelayInfrastructureCommand.CreateCommand());
+
+return await rootCommand.InvokeAsync(args).ConfigureAwait(false);
