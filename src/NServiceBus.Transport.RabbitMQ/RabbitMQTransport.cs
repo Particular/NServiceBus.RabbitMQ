72014bc967e47791a351259fca619af36e3fb3aa
Brandon Ording <bording@gmail.com>
2022-05-03 17:28:47 -0400

    Bump version to 8.0 (#991)

--- src/NServiceBus.Transport.RabbitMQ/RabbitMQTransport.cs
+++ src/NServiceBus.Transport.RabbitMQ/RabbitMQTransport.cs
@@ -262,8 +262,8 @@
         /// </summary>
         [ObsoleteEx(
             Message = "Inject the ITransportAddressResolver type to access the address translation mechanism at runtime. See the NServiceBus version 8 upgrade guide for further details.",
-            TreatAsErrorFromVersion = "8",
-            RemoveInVersion = "9")]
+            TreatAsErrorFromVersion = "9",
+            RemoveInVersion = "10")]
 #pragma warning disable CS0672 // Member overrides obsolete member
         public override string ToTransportAddress(QueueAddress address)
 #pragma warning restore CS0672 // Member overrides obsolete member
