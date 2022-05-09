c8e599e83bad1384151ad7a87f8d3c2015859ecf
kentdr <dan.kent@particular.net>
2022-05-09 19:16:00 -0400

    Add install-delay-infrastructure command

---
+++ src/CommandLine/InstallDelayInfrastructureCommand.cs
@@ -0,0 +1,20 @@
+﻿namespace NServiceBus.Transport.RabbitMQ.CommandLine
+{
+    using System.CommandLine;
+
+    class InstallDelayInfrastructureCommand
+    {
+        public static Command CreateCommand(Option<string> connectionStringOption)
+        {
+            var delayInstallerCommand = new Command("install-delay-infrastructure", "Create delay infrastructure queues and exchanges");
+            delayInstallerCommand.AddOption(connectionStringOption);
+
+            delayInstallerCommand.SetHandler((string connectionString) =>
+            {
+                CommandRunner.Run(connectionString, channel => DelayInfrastructure.Build(channel));
+            }, connectionStringOption);
+
+            return delayInstallerCommand;
+        }
+    }
+}
