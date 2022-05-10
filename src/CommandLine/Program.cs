03bb38c6de6fb6fe71efc581234a1a019ef4b8a9
kentdr <dan.kent@particular.net>
2022-05-10 09:27:15 -0400

    Move connection string option to a static class.

--- src/CommandLine/Program.cs
+++ src/CommandLine/Program.cs
@@ -1,18 +1,9 @@
 ﻿using System.CommandLine;
 using NServiceBus.Transport.RabbitMQ.CommandLine;
 
-const string ConnectionStringEnvironmentVariable = "RabbitMQTransport_ConnectionString";
-
-var connectionStringOption = new Option<string>(
-        name: "--connectionString",
-        description: $"Overrides environment variable '{ConnectionStringEnvironmentVariable}'",
-        getDefaultValue: () => Environment.GetEnvironmentVariable(ConnectionStringEnvironmentVariable) ?? string.Empty);
-
-connectionStringOption.AddAlias("-c");
-
 var rootCommand = new RootCommand("A .NET global tool to manage the RabbitMQ transport for NServiceBus endpoints");
 
-rootCommand.AddCommand(MigrateDelayInfrastructureCommand.CreateCommand(connectionStringOption));
-rootCommand.AddCommand(InstallDelayInfrastructureCommand.CreateCommand(connectionStringOption));
+rootCommand.AddCommand(MigrateDelayInfrastructureCommand.CreateCommand());
+rootCommand.AddCommand(InstallDelayInfrastructureCommand.CreateCommand());
 
 return await rootCommand.InvokeAsync(args).ConfigureAwait(false);
---
+++ src/CommandLine/SharedOptions.cs
@@ -0,0 +1,29 @@
+﻿namespace NServiceBus.Transport.RabbitMQ.CommandLine
+{
+    using System.CommandLine;
+
+    static class SharedOptions
+    {
+        const string ConnectionStringEnvironmentVariable = "RabbitMQTransport_ConnectionString";
+
+        static Option<string>? ConnectionStringOption = null;
+
+        public static Option<string> ConnectionString
+        {
+            get
+            {
+                if (ConnectionStringOption == null)
+                {
+                    ConnectionStringOption = new Option<string>(
+                        name: "--connectionString",
+                        description: $"Overrides environment variable '{ConnectionStringEnvironmentVariable}'",
+                        getDefaultValue: () => Environment.GetEnvironmentVariable(ConnectionStringEnvironmentVariable) ?? string.Empty);
+
+                    ConnectionStringOption.AddAlias("-c");
+                }
+
+                return ConnectionStringOption;
+            }
+        }
+    }
+}
