b09815518b6ec8b5274f15b53ea663d83061d49c
Andreas Öhlund <andreas.ohlund@particular.net>
2022-05-10 09:11:21 +0200

    Add skeleton command

--- src/CommandLine/Program.cs
+++ src/CommandLine/Program.cs
@@ -2,11 +2,11 @@
 using NServiceBus.Transport.RabbitMQ.CommandLine;
 
 var rootCommand = new RootCommand("A .NET global tool to manage the RabbitMQ transport for NServiceBus endpoints");
-
 var delaysSubCommand = new Command("delays", "A set of commands that provide functionality realted to the delay infrastructure");
 
 delaysSubCommand.AddCommand(MigrateDelayInfrastructureCommand.CreateCommand());
 delaysSubCommand.AddCommand(InstallDelayInfrastructureCommand.CreateCommand());
+delaysSubCommand.AddCommand(VerifySafeDelaysCommand.CreateCommand());
 
 rootCommand.AddCommand(delaysSubCommand);
 
