2eaf2a9c2d10a9d68695b3ba50e0db3b5ae8271e
Andreas Öhlund <andreas.ohlund@particular.net>
2022-05-10 09:11:21 +0200

    Add skeleton command

---
+++ src/CommandLine/VerifySafeDelaysCommand.cs
@@ -0,0 +1,29 @@
+﻿namespace NServiceBus.Transport.RabbitMQ.CommandLine
+{
+    using System;
+    using System.CommandLine;
+
+    class VerifySafeDelaysCommand
+    {
+        public static Command CreateCommand()
+        {
+            var verifyCommand = new Command("verify-safe-delays", "Verifies that the broker configuration allows for safe message delays.");
+
+            verifyCommand.SetHandler(async (CancellationToken cancellationToken) =>
+            {
+                var verifyProcess = new VerifySafeDelaysCommand();
+                await verifyProcess.Execute(cancellationToken).ConfigureAwait(false);
+
+            });
+
+            return verifyCommand;
+        }
+
+        public Task Execute(CancellationToken cancellationToken = default)
+        {
+            Console.WriteLine("yay");
+
+            return Task.CompletedTask;
+        }
+    }
+}
