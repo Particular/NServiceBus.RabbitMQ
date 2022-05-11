79acd08699074093052d23e3676d47915be3d9c9
kentdr <dan.kent@particular.net>
2022-05-11 09:55:03 -0400

    Remove folder links in csproj file.

--- src/CommandLine/Program.cs
+++ src/CommandLine/Program.cs
@@ -3,7 +3,11 @@ using NServiceBus.Transport.RabbitMQ.CommandLine;
 
 var rootCommand = new RootCommand("A .NET global tool to manage the RabbitMQ transport for NServiceBus endpoints");
 
-rootCommand.AddCommand(MigrateDelayInfrastructureCommand.CreateCommand());
-rootCommand.AddCommand(InstallDelayInfrastructureCommand.CreateCommand());
+var delaysSubCommand = new Command("delays", "A set of commands that provide functionality realted to the delay infrastructure");
+
+delaysSubCommand.AddCommand(MigrateDelayInfrastructureCommand.CreateCommand());
+delaysSubCommand.AddCommand(InstallDelayInfrastructureCommand.CreateCommand());
+
+rootCommand.AddCommand(delaysSubCommand);
 
 return await rootCommand.InvokeAsync(args).ConfigureAwait(false);
