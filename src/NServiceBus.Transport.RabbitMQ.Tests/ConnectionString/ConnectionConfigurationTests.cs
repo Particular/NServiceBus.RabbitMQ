1bb44710e90e569026dba5731669163839ad3d39
kentdr <dan.kent@particular.net>
2022-05-17 14:21:29 -0400

    Revert connection string tests to match release

--- src/NServiceBus.Transport.RabbitMQ.Tests/ConnectionString/ConnectionConfigurationTests.cs
+++ src/NServiceBus.Transport.RabbitMQ.Tests/ConnectionString/ConnectionConfigurationTests.cs
@@ -1,25 +1,22 @@
 ﻿namespace NServiceBus.Transport.RabbitMQ.Tests.ConnectionString
 {
     using System;
-    using System.IO;
     using NUnit.Framework;
+    using RabbitMQ;
 
     [TestFixture]
     public class ConnectionConfigurationTests
     {
-        static readonly string certPath = $"{TestContext.CurrentContext.TestDirectory}{Path.DirectorySeparatorChar}..{Path.DirectorySeparatorChar}..{Path.DirectorySeparatorChar}..{Path.DirectorySeparatorChar}myp12.p12";
+        const string connectionString =
+            "virtualHost=Copa;username=Copa;host=192.168.1.1:1234;password=abc_xyz;port=12345;requestedHeartbeat=3;" +
+            "retryDelay=01:02:03;useTls=true;certPath=/path/to/client/keycert.p12;certPassPhrase=abc123";
 
-        RabbitMQTransport CreateTransportDefinition(string connectionString)
-        {
-            return new RabbitMQTransport(Topology.Conventional, connectionString, QueueType.Classic);
-        }
+        ConnectionConfiguration defaults = ConnectionConfiguration.Create("host=localhost");
 
         [Test]
         public void Should_correctly_parse_full_connection_string()
         {
-            var connectionString = $"virtualHost=Copa;username=Copa;host=192.168.1.1:1234;password=abc_xyz;port=12345;requestedHeartbeat=3;retryDelay=01:02:03;useTls=true;certPath={certPath};certPassPhrase=abc123";
-            var transportDefinition = CreateTransportDefinition(connectionString);
-            var connectionConfiguration = transportDefinition.ConnectionConfiguration;
+            var connectionConfiguration = ConnectionConfiguration.Create(connectionString);
 
             Assert.AreEqual(connectionConfiguration.Host, "192.168.1.1");
             Assert.AreEqual(connectionConfiguration.Port, 1234);
@@ -28,19 +25,21 @@
             Assert.AreEqual(connectionConfiguration.Password, "abc_xyz");
             Assert.AreEqual(connectionConfiguration.RequestedHeartbeat, TimeSpan.FromSeconds(3));
             Assert.AreEqual(connectionConfiguration.RetryDelay, new TimeSpan(1, 2, 3)); //01:02:03
-            Assert.AreEqual("O=Particular, S=Some-State, C=PL", transportDefinition.ClientCertificate.Issuer);
+            Assert.AreEqual(connectionConfiguration.UseTls, true);
+            Assert.AreEqual(connectionConfiguration.CertPath, "/path/to/client/keycert.p12");
+            Assert.AreEqual(connectionConfiguration.CertPassphrase, "abc123");
         }
 
         [Test]
         public void Should_fail_if_host_is_not_present()
         {
-            Assert.Throws<NotSupportedException>(() => CreateTransportDefinition("virtualHost=Copa;username=Copa;password=abc_xyz;port=12345;requestedHeartbeat=3"));
+            Assert.Throws<NotSupportedException>(() => ConnectionConfiguration.Create("virtualHost=Copa;username=Copa;password=abc_xyz;port=12345;requestedHeartbeat=3"));
         }
 
         [Test]
         public void Should_parse_host()
         {
-            var connectionConfiguration = CreateTransportDefinition("host=host.one:1001;port=1002").ConnectionConfiguration;
+            var connectionConfiguration = ConnectionConfiguration.Create("host=host.one:1001;port=1002");
 
             Assert.AreEqual(connectionConfiguration.Host, "host.one");
             Assert.AreEqual(connectionConfiguration.Port, 1001);
@@ -49,7 +48,7 @@
         [Test]
         public void Should_parse_host_with_separate_port()
         {
-            var connectionConfiguration = CreateTransportDefinition("host=my.host.com;port=1234").ConnectionConfiguration;
+            var connectionConfiguration = ConnectionConfiguration.Create("host=my.host.com;port=1234");
 
             Assert.AreEqual(connectionConfiguration.Host, "my.host.com");
             Assert.AreEqual(connectionConfiguration.Port, 1234);
@@ -58,15 +57,16 @@
         [Test]
         public void Should_parse_host_without_port()
         {
-            var connectionConfiguration = CreateTransportDefinition("host=my.host.com").ConnectionConfiguration;
+            var connectionConfiguration = ConnectionConfiguration.Create("host=my.host.com");
 
             Assert.AreEqual(connectionConfiguration.Host, "my.host.com");
+            Assert.AreEqual(connectionConfiguration.Port, 5672);
         }
 
         [Test]
         public void Should_parse_the_hostname()
         {
-            var connectionConfiguration = CreateTransportDefinition("host=myHost").ConnectionConfiguration;
+            var connectionConfiguration = ConnectionConfiguration.Create("host=myHost");
 
             Assert.AreEqual("myHost", connectionConfiguration.Host);
         }
@@ -74,7 +74,7 @@
         [Test]
         public void Should_parse_the_password()
         {
-            var connectionConfiguration = CreateTransportDefinition("host=localhost;password=test").ConnectionConfiguration;
+            var connectionConfiguration = ConnectionConfiguration.Create("host=localhost;password=test");
 
             Assert.AreEqual("test", connectionConfiguration.Password);
         }
@@ -82,7 +82,7 @@
         [Test]
         public void Should_parse_the_port()
         {
-            var connectionConfiguration = CreateTransportDefinition("host=localhost;port=8181").ConnectionConfiguration;
+            var connectionConfiguration = ConnectionConfiguration.Create("host=localhost;port=8181");
 
             Assert.AreEqual(8181, connectionConfiguration.Port);
         }
@@ -90,7 +90,7 @@
         [Test]
         public void Should_parse_the_requestedHeartbeat()
         {
-            var connectionConfiguration = CreateTransportDefinition("host=localhost;requestedHeartbeat=5").ConnectionConfiguration;
+            var connectionConfiguration = ConnectionConfiguration.Create("host=localhost;requestedHeartbeat=5");
 
             Assert.AreEqual(TimeSpan.FromSeconds(5), connectionConfiguration.RequestedHeartbeat);
         }
@@ -98,7 +98,7 @@
         [Test]
         public void Should_parse_the_retry_delay()
         {
-            var connectionConfiguration = CreateTransportDefinition("host=localhost;retryDelay=00:00:10").ConnectionConfiguration;
+            var connectionConfiguration = ConnectionConfiguration.Create("host=localhost;retryDelay=00:00:10");
 
             Assert.AreEqual(TimeSpan.FromSeconds(10), connectionConfiguration.RetryDelay);
         }
@@ -106,7 +106,7 @@
         [Test]
         public void Should_parse_the_username()
         {
-            var connectionConfiguration = CreateTransportDefinition("host=localhost;username=test").ConnectionConfiguration;
+            var connectionConfiguration = ConnectionConfiguration.Create("host=localhost;username=test");
 
             Assert.AreEqual("test", connectionConfiguration.UserName);
         }
@@ -114,7 +114,7 @@
         [Test]
         public void Should_parse_the_virtual_hostname()
         {
-            var connectionConfiguration = CreateTransportDefinition("host=localhost;virtualHost=myVirtualHost").ConnectionConfiguration;
+            var connectionConfiguration = ConnectionConfiguration.Create("host=localhost;virtualHost=myVirtualHost");
 
             Assert.AreEqual("myVirtualHost", connectionConfiguration.VirtualHost);
         }
@@ -122,22 +122,117 @@
         [Test]
         public void Should_parse_use_tls()
         {
-            var connectionConfiguration = CreateTransportDefinition("host=localhost;useTls=true").ConnectionConfiguration;
+            var connectionConfiguration = ConnectionConfiguration.Create("host=localhost;useTls=true");
 
             Assert.AreEqual(true, connectionConfiguration.UseTls);
+            Assert.AreEqual(5671, connectionConfiguration.Port);
         }
+
         [Test]
         public void Should_parse_the_cert_path()
         {
-            var connectionConfiguration = CreateTransportDefinition($"host=localhost;certPath={certPath};certPassphrase=abc123");
+            var connectionConfiguration = ConnectionConfiguration.Create("host=localhost;certPath=/path/keyfile.p12");
 
-            Assert.AreEqual("O=Particular, S=Some-State, C=PL", connectionConfiguration.ClientCertificate.Issuer);
+            Assert.AreEqual("/path/keyfile.p12", connectionConfiguration.CertPath);
+        }
+
+        [Test]
+        public void Should_parse_the_cert_passphrase()
+        {
+            var connectionConfiguration = ConnectionConfiguration.Create("host=localhost;certPassphrase=abc123");
+
+            Assert.AreEqual("abc123", connectionConfiguration.CertPassphrase);
         }
 
         [Test]
         public void Should_throw_on_malformed_string()
         {
-            Assert.Throws<ArgumentException>(() => CreateTransportDefinition("not a well formed name value pair;"));
+            Assert.Throws<ArgumentException>(() => ConnectionConfiguration.Create("not a well formed name value pair;"));
+        }
+
+        [Test]
+        public void Should_list_all_invalid_options()
+        {
+            var connectionString =
+                "host=:notaport1,host=localhost2;" +
+                "port=notaport2;" +
+                "useTls=notusetls;" +
+                "requestedHeartbeat=notaheartbeat;" +
+                "retryDelay=notaretrydelay;" +
+                "usePublisherConfirms=true;" +
+                "prefetchcount=100;" +
+                "maxWaitTimeForConfirms=02:03:39;" +
+                "dequeuetimeout=1;";
+
+            var exception = Assert.Throws<NotSupportedException>(() =>
+                ConnectionConfiguration.Create(connectionString));
+
+            Assert.That(exception.Message, Does.Contain("Multiple hosts are no longer supported"));
+            Assert.That(exception.Message, Does.Contain("consider using a load balancer"));
+            Assert.That(exception.Message, Does.Contain("Empty host name in 'host' connection string option."));
+            Assert.That(exception.Message, Does.Contain("'notaport1' is not a valid Int32 value for the port in the 'host' connection string option."));
+            Assert.That(exception.Message, Does.Contain("'notaport2' is not a valid Int32 value for the 'port' connection string option."));
+            Assert.That(exception.Message, Does.Contain("'notusetls' is not a valid Boolean value for the 'useTls' connection string option."));
+            Assert.That(exception.Message, Does.Contain("'notaheartbeat' is not a valid UInt16 value for the 'requestedHeartbeat' connection string option."));
+            Assert.That(exception.Message, Does.Contain("'notaretrydelay' is not a valid TimeSpan value for the 'retryDelay' connection string option."));
+            Assert.That(exception.Message, Does.Contain("The 'UsePublisherConfirms' connection string option has been removed"));
+            Assert.That(exception.Message, Does.Contain("The 'PrefetchCount' connection string option has been removed"));
+            Assert.That(exception.Message, Does.Contain("The 'MaxWaitTimeForConfirms' connection string option has been removed"));
+            Assert.That(exception.Message, Does.Contain("The 'DequeueTimeout' connection string option has been removed"));
+        }
+
+        [Test]
+        public void Should_set_default_port()
+        {
+            Assert.AreEqual(defaults.Port, 5672);
+        }
+
+        [Test]
+        public void Should_set_default_virtual_host()
+        {
+            Assert.AreEqual(defaults.VirtualHost, "/");
+        }
+
+        [Test]
+        public void Should_set_default_username()
+        {
+            Assert.AreEqual(defaults.UserName, "guest");
+        }
+
+        [Test]
+        public void Should_set_default_password()
+        {
+            Assert.AreEqual(defaults.Password, "guest");
+        }
+
+        [Test]
+        public void Should_set_default_requested_heartbeat()
+        {
+            Assert.AreEqual(defaults.RequestedHeartbeat, TimeSpan.FromSeconds(60));
+        }
+
+        [Test]
+        public void Should_set_default_retry_delay()
+        {
+            Assert.AreEqual(defaults.RetryDelay, TimeSpan.FromSeconds(10));
+        }
+
+        [Test]
+        public void Should_set_default_use_tls()
+        {
+            Assert.AreEqual(defaults.UseTls, false);
+        }
+
+        [Test]
+        public void Should_set_default_cert_path()
+        {
+            Assert.AreEqual(defaults.CertPath, "");
+        }
+
+        [Test]
+        public void Should_set_default_retry_cert_passphrase()
+        {
+            Assert.AreEqual(defaults.CertPassphrase, null);
         }
     }
 }
