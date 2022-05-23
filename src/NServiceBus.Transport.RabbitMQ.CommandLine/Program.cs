6b84b024cd8f777cb7b0f599697234f27b0fce24
Brandon Ording <bording@gmail.com>
2022-05-23 17:05:40 -0400

    Adjust command order

--- src/NServiceBus.Transport.RabbitMQ.CommandLine/Program.cs
+++ src/NServiceBus.Transport.RabbitMQ.CommandLine/Program.cs
@@ -4,8 +4,8 @@ using NServiceBus.Transport.RabbitMQ.CommandLine;
 var rootCommand = new RootCommand("A .NET global tool to manage the RabbitMQ transport for NServiceBus endpoints");
 var delaysSubCommand = new Command("delays", "A set of commands that provide functionality realted to the delay infrastructure");
 
-delaysSubCommand.AddCommand(DelaysMigrateCommand.CreateCommand());
 delaysSubCommand.AddCommand(DelaysCreateCommand.CreateCommand());
+delaysSubCommand.AddCommand(DelaysMigrateCommand.CreateCommand());
 delaysSubCommand.AddCommand(DelaysVerifyCommand.CreateCommand());
 
 rootCommand.AddCommand(delaysSubCommand);
