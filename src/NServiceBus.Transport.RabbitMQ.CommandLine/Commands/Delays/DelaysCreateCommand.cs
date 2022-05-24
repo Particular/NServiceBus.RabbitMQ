d68364738a87cccd270523179d7651be850abb27
kentdr <dan.kent@particular.net>
2022-05-24 14:16:34 -0400

    Add console output to delays create command

--- src/NServiceBus.Transport.RabbitMQ.CommandLine/Commands/Delays/DelaysCreateCommand.cs
+++ src/NServiceBus.Transport.RabbitMQ.CommandLine/Commands/Delays/DelaysCreateCommand.cs
@@ -7,7 +7,7 @@
     {
         public static Command CreateCommand()
         {
-            var command = new Command("create", "Create delay infrastructure queues and exchanges");
+            var command = new Command("create", "Create v2 delay infrastructure queues and exchanges");
 
             var connectionStringOption = SharedOptions.CreateConnectionStringOption();
             var certPathOption = SharedOptions.CreateCertPathOption();
@@ -17,7 +17,7 @@
             command.AddOption(certPathOption);
             command.AddOption(certPassphraseOption);
 
-            command.SetHandler((string connectionString, string certPath, string certPassphrase) =>
+            command.SetHandler(async (string connectionString, string certPath, string certPassphrase, CancellationToken cancellationToken) =>
             {
                 X509Certificate2? certificate = null;
 
@@ -26,10 +26,38 @@
                     certificate = new X509Certificate2(certPath, certPassphrase);
                 }
 
-                CommandRunner.Run(connectionString, certificate, channel => DelayInfrastructure.Build(channel));
+                var createProcess = new DelaysCreateCommand(connectionString, certificate);
+
+                await createProcess.Run(cancellationToken).ConfigureAwait(false);
+
             }, connectionStringOption, certPathOption, certPassphraseOption);
 
             return command;
         }
+
+        public DelaysCreateCommand(string connectionString, X509Certificate2? certificate)
+        {
+            this.connectionString = connectionString;
+            this.certificate = certificate;
+        }
+
+        public Task Run(CancellationToken cancellationToken = default)
+        {
+            try
+            {
+                CommandRunner.Run(connectionString, certificate, channel => DelayInfrastructure.Build(channel));
+
+                Console.WriteLine("Delay infrastructure v2 created successfully");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Fail: {ex.Message}");
+            }
+
+            return Task.CompletedTask;
+        }
+
+        readonly string connectionString;
+        readonly X509Certificate2? certificate;
     }
 }
