43668d35c573c1563f4708f52638627398ee7720
Andreas Öhlund <andreas.ohlund@particular.net>
2022-05-25 11:16:24 +0200

    Add test for check if endpoint queue already is quorum

--- src/NServiceBus.Transport.RabbitMQ.CommandLine.Tests/MigrateEndpoint/EndpointMigrationTests.cs
+++ src/NServiceBus.Transport.RabbitMQ.CommandLine.Tests/MigrateEndpoint/EndpointMigrationTests.cs
@@ -1,6 +1,7 @@
 ﻿namespace NServiceBus.Transport.RabbitMQ.CommandLine.Tests.MigrateEndpoint
 {
     using System;
+    using System.Collections.Generic;
     using global::RabbitMQ.Client.Exceptions;
     using NUnit.Framework;
 
@@ -17,6 +18,36 @@
 #pragma warning disable CS8602 // Dereference of a possibly null reference.
             StringAssert.Contains(endpointName, ex.Message);
 #pragma warning restore CS8602 // Dereference of a possibly null reference.
+        }
+
+        [Test]
+        public void Should_blow_up_when_endpoint_queue_already_is_quorum()
+        {
+            var migrationCommand = new MigrateEndpointCommand();
+            var endpointName = "EndpointThatIsAlreadyMigrated";
+
+            CreateQueue(endpointName, quorum: true);
+
+            var ex = Assert.ThrowsAsync<Exception>(async () => await migrationCommand.Run(endpointName, ConnectionString, Topology.Conventional, true).ConfigureAwait(false));
+
+#pragma warning disable CS8602 // Dereference of a possibly null reference.
+            StringAssert.Contains(endpointName, ex.Message);
+#pragma warning restore CS8602 // Dereference of a possibly null reference.
+        }
+
+        void CreateQueue(string queueName, bool quorum)
+        {
+            CommandRunner.Run(ConnectionString, channel =>
+            {
+                var queueArguments = new Dictionary<string, object>();
+
+                if (quorum)
+                {
+                    queueArguments.Add("x-queue-type", "quorum");
+                }
+
+                channel.QueueDeclare(queueName, true, false, false, queueArguments);
+            });
         }
 
         static string ConnectionString = Environment.GetEnvironmentVariable("RabbitMQTransport_ConnectionString") ?? "host=localhost";
