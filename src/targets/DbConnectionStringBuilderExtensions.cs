5a01c57f848a19822054f06d3c79e1ae26d8ba92
Brandon Ording <bording@gmail.com>
2022-06-07 13:21:32 -0400

    Remove unused file

--- src/targets/DbConnectionStringBuilderExtensions.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Data.Common;
-
-static class DbConnectionStringBuilderExtensions
-{
-    public static string GetOrDefault(this DbConnectionStringBuilder builder, string key, string defaultValue) =>
-        builder.TryGetValue(key, out var value) ? value.ToString() : defaultValue;
-}
-
