311852f8416a5de7a4bcd320e8d09aa2acc9c149
Brandon Ording <bording@gmail.com>
2022-06-10 16:05:38 -0400

    React to beta 4 changes

---
+++ src/NServiceBus.Transport.RabbitMQ.CommandLine/Bind.cs
@@ -0,0 +1,17 @@
+﻿namespace NServiceBus.Transport.RabbitMQ.CommandLine
+{
+    using System.CommandLine.Binding;
+    using Microsoft.Extensions.DependencyInjection;
+
+    static class Bind
+    {
+        public static ServiceProviderBinder<T> FromServiceProvider<T>() => ServiceProviderBinder<T>.Instance;
+
+        internal class ServiceProviderBinder<T> : BinderBase<T>
+        {
+            public static ServiceProviderBinder<T> Instance { get; } = new();
+
+            protected override T GetBoundValue(BindingContext bindingContext) => (T)bindingContext.GetRequiredService(typeof(T));
+        }
+    }
+}
