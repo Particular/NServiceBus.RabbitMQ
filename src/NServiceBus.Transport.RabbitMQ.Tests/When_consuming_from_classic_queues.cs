dd4b72afb216a7fc05de94be8d4cd1d67125db69
Andreas Öhlund <andreas.ohlund@particular.net>
2023-04-12 08:51:17 +0200

    Add tests for headers being null

---
+++ src/NServiceBus.Transport.RabbitMQ.Tests/When_consuming_from_classic_queues.cs
@@ -0,0 +1,56 @@
+﻿namespace NServiceBus.Transport.RabbitMQ.Tests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using global::RabbitMQ.Client.Events;
+    using NUnit.Framework;
+
+    [TestFixture]
+    class When_consuming_from_classic_queues : RabbitMqContext
+    {
+        [SetUp]
+        public override Task SetUp()
+        {
+            queueType = QueueType.Classic;
+            return base.SetUp();
+        }
+
+        [Test]
+        public async Task Header_collection_is_null_for_headerless_messages()
+        {
+            var message = new OutgoingMessage(Guid.NewGuid().ToString(), new Dictionary<string, string>(), new byte[0]);
+            var headerCollectionWasNull = new TaskCompletionSource<bool>();
+
+            OnMessage = (mc, __) =>
+            {
+                var basicDeliverEventArgs = mc.Extensions.Get<BasicDeliverEventArgs>();
+                headerCollectionWasNull.SetResult(basicDeliverEventArgs.BasicProperties.Headers == null);
+
+                return Task.CompletedTask;
+            };
+
+            using (var connection = connectionFactory.CreatePublishConnection())
+            using (var channel = connection.CreateModel())
+            {
+                var properties = channel.CreateBasicProperties();
+
+                properties.MessageId = message.MessageId;
+
+                channel.BasicPublish(string.Empty, ReceiverQueue, false, properties, message.Body);
+
+                if (await Task.WhenAny(headerCollectionWasNull.Task, Task.Delay(IncomingMessageTimeout)) != headerCollectionWasNull.Task)
+                {
+                    Assert.Fail("Message receive timed out");
+                }
+
+                var headersWasNull = await headerCollectionWasNull.Task;
+                Assert.True(headersWasNull, "Header collection is null for classic queues due to a client bug");
+            }
+        }
+
+        class MyMessage
+        {
+        }
+    }
+}
--- src/NServiceBus.Transport.RabbitMQ.Tests/When_consuming_messages.cs
+++ src/NServiceBus.Transport.RabbitMQ.Tests/When_consuming_messages.cs
@@ -3,6 +3,7 @@
     using System;
     using System.Collections.Generic;
     using System.Threading.Tasks;
+    using global::RabbitMQ.Client.Events;
     using NUnit.Framework;
     using Routing;
 
@@ -102,6 +103,39 @@
                 var wasHandled = await handled.Task;
                 Assert.True(wasHandled, "Error handler should be called after retry");
                 Assert.AreEqual(1, numRetries, "Message should be retried once");
+            }
+        }
+
+        [Test]
+        public async Task Header_collection_is_not_null_for_headerless_messages()
+        {
+            var message = new OutgoingMessage(Guid.NewGuid().ToString(), new Dictionary<string, string>(), new byte[0]);
+            var headerCollectionWasNull = new TaskCompletionSource<bool>();
+
+            OnMessage = (mc, __) =>
+            {
+                var basicDeliverEventArgs = mc.Extensions.Get<BasicDeliverEventArgs>();
+                headerCollectionWasNull.SetResult(basicDeliverEventArgs.BasicProperties.Headers == null);
+
+                return Task.CompletedTask;
+            };
+
+            using (var connection = connectionFactory.CreatePublishConnection())
+            using (var channel = connection.CreateModel())
+            {
+                var properties = channel.CreateBasicProperties();
+
+                properties.MessageId = message.MessageId;
+
+                channel.BasicPublish(string.Empty, ReceiverQueue, false, properties, message.Body);
+
+                if (await Task.WhenAny(headerCollectionWasNull.Task, Task.Delay(IncomingMessageTimeout)) != headerCollectionWasNull.Task)
+                {
+                    Assert.Fail("Message receive timed out");
+                }
+
+                var headersWasNull = await headerCollectionWasNull.Task;
+                Assert.False(headersWasNull, "Header collection should not be null even if message has no headers");
             }
         }
 
