15dd62213d8ea61e7e11d44bfd49ea1373c58e23
Brandon Ording <bording@gmail.com>
2023-08-28 12:45:50 -0400

    Prepare repo for .NET 8 (#1292)
    * Bump major version
    
    * Adjust obsoletes for new major
    
    * Adjust PreObsoletes
    
    * Update to NServiceBus 9
    
    * Update dependencies
    
    * Update Test SDK
    
    * Update workflows
    
    * Update API approval file

--- src/NServiceBus.Transport.RabbitMQ/RabbitMQTransport.cs
+++ src/NServiceBus.Transport.RabbitMQ/RabbitMQTransport.cs
@@ -184,16 +184,44 @@
             return Task.FromResult<TransportInfrastructure>(infra);
         }
 
-#pragma warning disable CS0672 // Member overrides obsolete member
-        /// <inheritdoc />
-        [ObsoleteEx(
-            Message = "Inject the ITransportAddressResolver type to access the address translation mechanism at runtime. See the NServiceBus version 8 upgrade guide for further details.",
-            TreatAsErrorFromVersion = "9",
-            RemoveInVersion = "10")]
-        public override string ToTransportAddress(QueueAddress address) => RabbitMQTransportInfrastructure.TranslateAddress(address);
-#pragma warning restore CS0672 // Member overrides obsolete member
-
         /// <inheritdoc />
         public override IReadOnlyCollection<TransportTransactionMode> GetSupportedTransactionModes() => new[] { TransportTransactionMode.ReceiveOnly };
+
+        // Remove all Legacy API stuff below when PreObsoletes are converted 
+
+        internal string LegacyApiConnectionString { get; set; }
+
+        internal Func<bool, IRoutingTopology> TopologyFactory { get; set; }
+
+        internal bool UseDurableExchangesAndQueues { get; set; } = true;
+
+        bool legacyMode;
+
+        internal RabbitMQTransport() : base(TransportTransactionMode.ReceiveOnly, true, true, true)
+        {
+            legacyMode = true;
+        }
+
+        void ValidateAndApplyLegacyConfiguration()
+        {
+            if (!legacyMode)
+            {
+                return;
+            }
+
+            if (TopologyFactory == null)
+            {
+                throw new Exception("A routing topology must be configured with one of the 'EndpointConfiguration.UseTransport<RabbitMQTransport>().UseXXXXRoutingTopology()` methods. Most new projects should use the Conventional routing topology.");
+            }
+
+            RoutingTopology = TopologyFactory(UseDurableExchangesAndQueues);
+
+            if (string.IsNullOrEmpty(LegacyApiConnectionString))
+            {
+                throw new Exception("A connection string must be configured with 'EndpointConfiguration.UseTransport<RabbitMQTransport>().ConnectionString()` method.");
+            }
+
+            ConnectionConfiguration = ConnectionConfiguration.Create(LegacyApiConnectionString);
+        }
     }
 }
--- src/NServiceBus.Transport.RabbitMQ/obsoletes-v9.cs
+++ src/NServiceBus.Transport.RabbitMQ/obsoletes-v9.cs
@@ -3,73 +3,19 @@
 namespace NServiceBus
 {
     using System;
-    using Transport.RabbitMQ;
-
-    public static partial class RabbitMQTransportSettingsExtensions
-    {
-        [ObsoleteEx(
-            Message = "The transport no longer has configurable settings for delayed delivery.",
-            TreatAsErrorFromVersion = "8",
-            RemoveInVersion = "9")]
-        public static DelayedDeliverySettings DelayedDelivery(this TransportExtensions<RabbitMQTransport> transportExtensions)
-        {
-            throw new NotImplementedException();
-        }
-    }
-
-    [ObsoleteEx(
-        Message = "The transport no longer has configurable settings for delayed delivery.",
-        TreatAsErrorFromVersion = "8",
-        RemoveInVersion = "9")]
-    public class DelayedDeliverySettings
-    {
-        [ObsoleteEx(
-            Message = "The TimeoutManager has been removed from NServiceBus 8. See the upgrade guide for details on how to use the timeout migration tool.",
-            TreatAsErrorFromVersion = "8",
-            RemoveInVersion = "9")]
-        public DelayedDeliverySettings EnableTimeoutManager()
-        {
-            throw new NotImplementedException();
-        }
-    }
+    using NServiceBus.Transport;
 
     public partial class RabbitMQTransport
     {
-        internal string LegacyApiConnectionString { get; set; }
-
-        internal Func<bool, IRoutingTopology> TopologyFactory { get; set; }
-
-        internal bool UseDurableExchangesAndQueues { get; set; } = true;
-
-        bool legacyMode;
-
-        internal RabbitMQTransport() : base(TransportTransactionMode.ReceiveOnly, true, true, true)
-        {
-            legacyMode = true;
-        }
-
-        void ValidateAndApplyLegacyConfiguration()
-        {
-            if (!legacyMode)
-            {
-                return;
-            }
-
-            if (TopologyFactory == null)
-            {
-                throw new Exception("A routing topology must be configured with one of the 'EndpointConfiguration.UseTransport<RabbitMQTransport>().UseXXXXRoutingTopology()` methods. Most new projects should use the Conventional routing topology.");
-            }
-
-            RoutingTopology = TopologyFactory(UseDurableExchangesAndQueues);
-
-            if (string.IsNullOrEmpty(LegacyApiConnectionString))
-            {
-                throw new Exception("A connection string must be configured with 'EndpointConfiguration.UseTransport<RabbitMQTransport>().ConnectionString()` method.");
-            }
-
-            ConnectionConfiguration = ConnectionConfiguration.Create(LegacyApiConnectionString);
-        }
+        [ObsoleteEx(
+            Message = "Inject the ITransportAddressResolver type to access the address translation mechanism at runtime. See the NServiceBus version 8 upgrade guide for further details.",
+            TreatAsErrorFromVersion = "9",
+            RemoveInVersion = "10")]
+#pragma warning disable CS0672 // Member overrides obsolete member
+        public override string ToTransportAddress(QueueAddress address) => throw new NotImplementedException();
+#pragma warning restore CS0672 // Member overrides obsolete member
     }
+
 }
 
 #pragma warning restore 1591
