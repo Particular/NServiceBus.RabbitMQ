1ba584ae2960b8191f30360560c0be1a4bcdf390
Tim Bussmann <timbussmann@users.noreply.github.com>
2023-09-06 08:03:03 +0000

    Don't abort connection in ChangeConcurrency (#1258)

---
+++ src/NServiceBus.Transport.RabbitMQ.TransportTests/When_changing_concurrency.cs
@@ -0,0 +1,93 @@
+﻿namespace NServiceBus.Transport.RabbitMQ.TransportTests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using NServiceBus.TransportTests;
+    using NUnit.Framework;
+
+    public class When_changing_concurrency : NServiceBusTransportTest
+    {
+        [TestCase(TransportTransactionMode.None)]
+        [TestCase(TransportTransactionMode.ReceiveOnly)]
+        [TestCase(TransportTransactionMode.SendsAtomicWithReceive)]
+        [TestCase(TransportTransactionMode.TransactionScope)]
+        public async Task Should_complete_current_message(TransportTransactionMode transactionMode)
+        {
+            var triggeredChangeConcurrency = CreateTaskCompletionSource();
+            Task concurrencyChanged = null;
+            int invocationCounter = 0;
+
+            await StartPump(async (context, ct) =>
+                {
+                    Interlocked.Increment(ref invocationCounter);
+
+                    concurrencyChanged = Task.Run(async () =>
+                    {
+                        var task = receiver.ChangeConcurrency(new PushRuntimeSettings(1), ct);
+                        triggeredChangeConcurrency.SetResult();
+                        await task;
+                    }, ct);
+
+                    await triggeredChangeConcurrency.Task;
+
+                }, (_, _) =>
+                {
+                    Assert.Fail("Message processing should not fail");
+                    return Task.FromResult(ErrorHandleResult.RetryRequired);
+                },
+                transactionMode);
+
+            await SendMessage(InputQueueName);
+            await concurrencyChanged;
+            await StopPump();
+            Assert.AreEqual(1, invocationCounter, "message should successfully complete on first processing attempt");
+        }
+
+        [TestCase(TransportTransactionMode.None)]
+        [TestCase(TransportTransactionMode.ReceiveOnly)]
+        [TestCase(TransportTransactionMode.SendsAtomicWithReceive)]
+        [TestCase(TransportTransactionMode.TransactionScope)]
+        public async Task Should_dispatch_messages_from_on_error(TransportTransactionMode transactionMode)
+        {
+            int invocationCounter = 0;
+
+            var triggeredChangeConcurrency = CreateTaskCompletionSource();
+            var sentMessageReceived = CreateTaskCompletionSource();
+
+            await StartPump((context, _) =>
+                {
+                    Interlocked.Increment(ref invocationCounter);
+                    if (context.Headers.TryGetValue("FromOnError", out var value) && value == bool.TrueString)
+                    {
+                        sentMessageReceived.SetResult();
+                    }
+
+                    throw new Exception("triggering recoverability pipeline");
+                }, async (context, ct) =>
+                {
+                    // same behavior as delayed retries
+                    _ = Task.Run(async () =>
+                    {
+                        var task = receiver.ChangeConcurrency(new PushRuntimeSettings(1), ct);
+                        triggeredChangeConcurrency.SetResult();
+                        await task;
+                    }, ct);
+
+                    await triggeredChangeConcurrency.Task;
+                    await SendMessage(InputQueueName,
+                        new Dictionary<string, string>() { { "FromOnError", bool.TrueString } },
+                        context.TransportTransaction, cancellationToken: ct);
+                    return ErrorHandleResult.Handled;
+                },
+                transactionMode);
+
+            await SendMessage(InputQueueName);
+
+            await sentMessageReceived.Task;
+            await StopPump();
+            Assert.AreEqual(2, invocationCounter, "there should be exactly 2 messages (initial message and new message from onError pipeline)");
+        }
+    }
+}
---
+++ src/NServiceBus.Transport.RabbitMQ.TransportTests/When_restarting_endpoint.cs
@@ -0,0 +1,116 @@
+﻿namespace NServiceBus.TransportTests
+{
+    using System;
+    using System.Collections.Concurrent;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using NUnit.Framework;
+    using Transport;
+
+    public class When_restarting_endpoint : NServiceBusTransportTest
+    {
+        [TestCase(TransportTransactionMode.None)]
+        [TestCase(TransportTransactionMode.ReceiveOnly)]
+        [TestCase(TransportTransactionMode.SendsAtomicWithReceive)]
+        [TestCase(TransportTransactionMode.TransactionScope)]
+        public async Task Should_handle_multiple_stop_calls_to_started_receiver(TransportTransactionMode transactionMode)
+        {
+            await StartPump(
+                (_, _) => Task.CompletedTask,
+                (_, _) => Task.FromResult(ErrorHandleResult.Handled),
+                transactionMode);
+
+            await receiver.StopReceive();
+            Assert.DoesNotThrowAsync(() => receiver.StopReceive());
+        }
+
+        [TestCase(TransportTransactionMode.None)]
+        [TestCase(TransportTransactionMode.ReceiveOnly)]
+        [TestCase(TransportTransactionMode.SendsAtomicWithReceive)]
+        [TestCase(TransportTransactionMode.TransactionScope)]
+        public async Task Should_allow_restarting_receivers(TransportTransactionMode transactionMode)
+        {
+            var messageReceived = CreateTaskCompletionSource();
+
+            await StartPump((_, _) =>
+                {
+                    messageReceived.SetResult();
+                    return Task.CompletedTask;
+                },
+                (_, _) => Task.FromResult(ErrorHandleResult.Handled), transactionMode);
+
+            await receiver.StopReceive();
+            await receiver.StartReceive();
+
+            await SendMessage(InputQueueName);
+            await messageReceived.Task;
+        }
+
+        [TestCase(TransportTransactionMode.None)]
+        [TestCase(TransportTransactionMode.ReceiveOnly)]
+        [TestCase(TransportTransactionMode.SendsAtomicWithReceive)]
+        [TestCase(TransportTransactionMode.TransactionScope)]
+        public async Task Should_gracefully_restart_processing(TransportTransactionMode transactionMode)
+        {
+            var receivedMessages = new ConcurrentQueue<string>();
+            var pumpStopTriggered = CreateTaskCompletionSource();
+            var followupMessageReceived = CreateTaskCompletionSource();
+
+            await StartPump(async (context, token) =>
+                {
+                    var messageType = context.Headers["Type"];
+                    receivedMessages.Enqueue(messageType);
+                    TestContext.WriteLine("Received message " + messageType);
+
+                    switch (messageType)
+                    {
+                        case "Start":
+
+                            // run async because the pump might block the return until all inflight messages are processed.
+                            var stopTask = Task.Run(async () =>
+                            {
+                                var t = receiver.StopReceive(token);
+                                pumpStopTriggered.SetResult();
+                                await t;
+                                TestContext.WriteLine("Stopped receiver");
+                            }, token);
+
+                            await pumpStopTriggered.Task;
+                            // send message after StopReceive has been called
+                            await SendMessage(InputQueueName,
+                                new Dictionary<string, string>() { { "Type", "Followup" } },
+                                context.TransportTransaction, cancellationToken: token);
+
+                            // restart pump after endpoint has been stopped and the message has been sent
+                            _ = stopTask.ContinueWith(async _ =>
+                            {
+                                await receiver.StartReceive(token);
+                                TestContext.WriteLine("Started receiver");
+                            }, token);
+
+                            break;
+                        case "Followup":
+                            followupMessageReceived.SetResult();
+                            break;
+                        default:
+                            throw new ArgumentException();
+                    }
+                },
+                (context, _) =>
+                {
+                    Assert.Fail($"Message failed processing: {context.Exception}");
+                    return Task.FromResult(ErrorHandleResult.Handled);
+                }, transactionMode);
+
+            await SendMessage(InputQueueName, new Dictionary<string, string>() { { "Type", "Start" } });
+            await followupMessageReceived.Task;
+            await StopPump();
+
+            Assert.AreEqual(2, receivedMessages.Count);
+            Assert.IsTrue(receivedMessages.TryDequeue(out var firstMessageType));
+            Assert.AreEqual("Start", firstMessageType);
+            Assert.IsTrue(receivedMessages.TryDequeue(out var secondMessageType));
+            Assert.AreEqual("Followup", secondMessageType);
+        }
+    }
+}
