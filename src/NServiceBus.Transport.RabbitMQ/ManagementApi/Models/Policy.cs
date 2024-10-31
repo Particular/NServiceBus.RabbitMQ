28751b04fa1253fc1099ec9f5bcdfad883dac5a5
Andreas Bednarz <andreas.bednarz@particular.net>
2024-11-01 00:44:52 +1100

    Add policy related models for Management API

---
+++ src/NServiceBus.Transport.RabbitMQ/ManagementApi/Models/Policy.cs
@@ -0,0 +1,35 @@
+﻿#nullable enable
+
+namespace NServiceBus.Transport.RabbitMQ.ManagementApi.Models;
+
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Text.RegularExpressions;
+using NServiceBus.Transport.RabbitMQ.ManagementApi.Converters;
+
+class Policy
+{
+    [JsonPropertyName("vhost")]
+    public required string VirtualHost { get; set; }
+
+    [JsonPropertyName("name")]
+    public required string Name { get; set; }
+
+    [JsonPropertyName("pattern")]
+    public required Regex Pattern { get; set; }
+
+    [JsonConverter(typeof(PolicyTargetConverter))]
+    [JsonPropertyName("apply-to")]
+    public required PolicyTarget ApplyTo { get; set; }
+
+    [JsonPropertyName("definition")]
+    public required PolicyDefinition Definition { get; set; }
+
+    [JsonPropertyName("priority")]
+    public int Priority { get; set; }
+
+    [JsonExtensionData]
+    public IDictionary<string, JsonElement> ExtraProperties { get; } = new Dictionary<string, JsonElement>();
+}
+
