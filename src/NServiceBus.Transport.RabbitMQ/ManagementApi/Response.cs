431ad7b6d62ab3414555d4dddf953b4fb3cc1601
Andreas Bednarz <andreas.bednarz@particular.net>
2024-11-01 17:57:09 +1100

    Added Management Client method for getting Management API overview

---
+++ src/NServiceBus.Transport.RabbitMQ/ManagementApi/Response.cs
@@ -0,0 +1,12 @@
+﻿#nullable enable
+
+namespace NServiceBus.Transport.RabbitMQ.ManagementApi;
+
+using System.Diagnostics.CodeAnalysis;
+using System.Net;
+
+record Response<T>(HttpStatusCode StatusCode, string Reason, T Value)
+{
+    [MemberNotNullWhen(true, nameof(Value))]
+    public bool HasValue => StatusCode.IsSuccessStatusCode();
+}
