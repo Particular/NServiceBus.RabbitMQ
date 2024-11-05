a18ed4f76203dda77502f770294cbad8b8f561a6
Travis Nickels <travis.nickels@particular.net>
2024-11-05 15:18:40 -0800

    Suppress Fody obsolete usage error

--- src/NServiceBus.Transport.RabbitMQ/Administration/ManagementClient/Models/Queue.cs
+++ src/NServiceBus.Transport.RabbitMQ/Administration/ManagementClient/Models/Queue.cs
@@ -9,16 +9,22 @@ using NServiceBus.Transport.RabbitMQ.Administration.ManagementClient.Converters;
 
 class Queue
 {
-    [JsonRequired]
+    // This is because Fody is throwing an error when using the 'required' keyword that ctor has an obsoleteAttribute.
+    // https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/proposals/csharp-11.0/required-members#metadata-representation
+    [DoNotWarnAboutObsoleteUsage]
+    public Queue()
+    {
+    }
+
     [JsonPropertyName("name")]
-    public string? Name { get; set; }
+    public required string Name { get; set; }
 
     [JsonPropertyName("delivery_limit")]
     [JsonConverter(typeof(DeliveryLimitConverter))]
     public int DeliveryLimit { get; set; }
 
     [JsonPropertyName("effective_policy_definition")]
-    public PolicyDefinition? EffectivePolicyDefinition { get; set; }
+    public required PolicyDefinition EffectivePolicyDefinition { get; set; }
 
     [JsonPropertyName("policy")]
     public string? AppliedPolicyName { get; set; }
@@ -29,4 +35,3 @@ class Queue
     [JsonExtensionData]
     public IDictionary<string, JsonElement> ExtraProperties { get; } = new Dictionary<string, JsonElement>();
 }
-
