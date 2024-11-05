73c20db6a079c1d2b18dc6dc05f3b2b38a7f1422
Travis Nickels <travis.nickels@particular.net>
2024-11-04 17:51:00 -0800

    Simplify management API test for creating quorum queue

--- src/NServiceBus.Transport.RabbitMQ.Tests/Connection/ManagementConnection/When_connecting_to_the_rabbitmq_management_api.cs
+++ src/NServiceBus.Transport.RabbitMQ.Tests/Connection/ManagementConnection/When_connecting_to_the_rabbitmq_management_api.cs
@@ -1,74 +1,43 @@
-﻿namespace NServiceBus.Transport.RabbitMQ.Tests.Connection.ManagementConnection
+﻿#nullable enable
+
+namespace NServiceBus.Transport.RabbitMQ.Tests.Connection.ManagementConnection
 {
     using System;
     using System.Collections.Generic;
     using System.Net;
-    using System.Security.Cryptography.X509Certificates;
     using System.Threading.Tasks;
     using NServiceBus.Transport.RabbitMQ.Administration.ManagementClient;
     using NUnit.Framework;
-    using System.Threading;
+    using NUnit.Framework.Internal;
+    using global::RabbitMQ.Client;
+    using global::RabbitMQ.Client.Events;
+    using ConnectionFactory = ConnectionFactory;
+
 
     [TestFixture]
     class When_connecting_to_the_rabbitmq_management_api : RabbitMQTransport
     {
         ConnectionConfiguration connectionConfiguration = ConnectionConfiguration.Create("host=localhost");
-        protected IList<string> AdditionalReceiverQueues = [];
         protected QueueType queueType = QueueType.Quorum;
         protected string ReceiverQueue => GetTestQueueName("ManagementAPITestQueue");
-        protected string ErrorQueue => GetTestQueueName("error");
         protected string GetTestQueueName(string queueName) => $"{queueName}-{queueType}";
-        readonly List<(string hostName, int port, bool useTls)> additionalClusterNodes = [];
-        internal new IRoutingTopology RoutingTopology;
-        internal RabbitMQTransport transport;
 
         [SetUp]
         public async Task SetUp()
         {
-            var connectionString = Environment.GetEnvironmentVariable("RabbitMQTransport_ConnectionString") ?? "host=localhost";
+            var connectionFactory = new ConnectionFactory(ReceiverQueue, connectionConfiguration, null, !ValidateRemoteCertificate, UseExternalAuthMechanism, HeartbeatInterval, NetworkRecoveryInterval, []);
+            IConnection connection = await connectionFactory.CreateConnection(ReceiverQueue).ConfigureAwait(false);
+            var createChannelOptions = new CreateChannelOptions(publisherConfirmationsEnabled: false, publisherConfirmationTrackingEnabled: false);
+            var channel = await connection.CreateChannelAsync(createChannelOptions).ConfigureAwait(false);
+            var arguments = new Dictionary<string, object?> { { "x-queue-type", "quorum" } };
 
-            var useTls = connectionString.StartsWith("https", StringComparison.InvariantCultureIgnoreCase) || connectionString.StartsWith("amqps", StringComparison.InvariantCultureIgnoreCase);
+            _ = await channel.QueueDeclareAsync(queue: ReceiverQueue, durable: true, exclusive: false, autoDelete: false, arguments: arguments).ConfigureAwait(false);
 
-            connectionConfiguration = ConnectionConfiguration.Create(connectionString);
-            RoutingTopology = NServiceBus.RoutingTopology.Conventional(queueType).Create();
+            var consumer = new AsyncEventingBasicConsumer(channel);
+            consumer.ReceivedAsync += async (o, a) => await Task.Yield();
 
-            transport = new RabbitMQTransport(NServiceBus.RoutingTopology.Conventional(queueType), connectionString);
-
-            _ = await Initialize(new HostSettings(ReceiverQueue, ReceiverQueue, new StartupDiagnosticEntries(), (_, _, _) => { }, true),
-                [new ReceiveSettings(ReceiverQueue, new QueueAddress(ReceiverQueue), true, true, ErrorQueue)], [.. AdditionalReceiverQueues, ErrorQueue]);
-        }
-
-        public override async Task<TransportInfrastructure> Initialize(HostSettings hostSettings, ReceiveSettings[] receivers, string[] sendingAddresses, CancellationToken cancellationToken = default)
-        {
-            transport.ValidateAndApplyLegacyConfiguration();
-
-            X509Certificate2Collection certCollection = null;
-
-            if (ClientCertificate != null)
-            {
-                certCollection = new X509Certificate2Collection(ClientCertificate);
-            }
-
-            var connectionFactory = new ConnectionFactory(hostSettings.Name, connectionConfiguration, certCollection, !ValidateRemoteCertificate,
-                UseExternalAuthMechanism, HeartbeatInterval, NetworkRecoveryInterval, additionalClusterNodes);
-
-            //var managementClient = new ManagementClient(ConnectionConfiguration);
-
-            var channelProvider = new ChannelProvider(connectionFactory, NetworkRecoveryInterval, RoutingTopology);
-            await channelProvider.CreateConnection(cancellationToken).ConfigureAwait(false);
-
-            var converter = new MessageConverter(MessageIdStrategy);
-
-            var infra = new RabbitMQTransportInfrastructure(hostSettings, receivers, connectionFactory,
-                RoutingTopology, channelProvider, converter, null, TimeToWaitBeforeTriggeringCircuitBreaker,
-                PrefetchCountCalculation, NetworkRecoveryInterval, SupportsDelayedDelivery);
-
-            if (hostSettings.SetupInfrastructure)
-            {
-                await infra.SetupInfrastructure(sendingAddresses, cancellationToken).ConfigureAwait(false);
-            }
-
-            return infra;
+            var consumerTag = $"localhost - {ReceiverQueue}";
+            _ = await channel.BasicConsumeAsync(ReceiverQueue, true, consumerTag, consumer).ConfigureAwait(false);
         }
 
         [Test]
