47e888844824e482c0afc877d205ae99bafa9f06
Andreas Bednarz <andreas.bednarz@particular.net>
2024-11-06 14:23:32 +1100

    Move QueueArguments to the correct namespace

---
+++ src/NServiceBus.Transport.RabbitMQ/Administration/ManagementClient/Converters/QueueTypeConverter.cs
@@ -0,0 +1,35 @@
+﻿#nullable enable
+
+namespace NServiceBus.Transport.RabbitMQ.Administration.ManagementClient.Converters;
+
+using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using NServiceBus.Transport.RabbitMQ.Administration.ManagementClient.Models;
+
+class QueueTypeConverter : JsonConverter<QueueType>
+{
+    public override QueueType Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        var value = reader.GetString();
+        return value switch
+        {
+            "quorum" => QueueType.Quorum,
+            "classic" => QueueType.Classic,
+            "stream" => QueueType.Stream,
+            _ => throw new JsonException($"Unknown QueueType: {value}")
+        };
+    }
+
+    public override void Write(Utf8JsonWriter writer, QueueType queueType, JsonSerializerOptions options)
+    {
+        var value = queueType switch
+        {
+            QueueType.Quorum => "quorum",
+            QueueType.Classic => "classic",
+            QueueType.Stream => "stream",
+            _ => throw new ArgumentOutOfRangeException(nameof(queueType), $"QueueType value out of range: {queueType}")
+        };
+        writer.WriteStringValue(value);
+    }
+}
