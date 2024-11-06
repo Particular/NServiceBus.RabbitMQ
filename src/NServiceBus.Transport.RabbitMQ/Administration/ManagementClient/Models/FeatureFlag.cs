e74eff85ce1c032239cba22f1792bff8e80da983
Andreas Bednarz <andreas.bednarz@particular.net>
2024-11-06 23:02:56 +1100

    Extract broker verification into its own class

--- src/NServiceBus.Transport.RabbitMQ/Administration/ManagementClient/Models/FeatureFlag.cs
+++ src/NServiceBus.Transport.RabbitMQ/Administration/ManagementClient/Models/FeatureFlag.cs
@@ -10,26 +10,15 @@ using NServiceBus.Transport.RabbitMQ.Administration.ManagementClient.Converters;
 // This is to prevent Fody throwing an error on classes with `required` properties (since the compiler marks the default constructor with an `[Obsolete]` attribute)
 // https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/proposals/csharp-11.0/required-members#metadata-representation
 [method: DoNotWarnAboutObsoleteUsage]
-class Policy()
+class FeatureFlag()
 {
-    [JsonPropertyName("vhost")]
-    public string VirtualHost { get; set; } = "/";
-
+    [JsonRequired]
     [JsonPropertyName("name")]
     public required string Name { get; set; }
 
-    [JsonPropertyName("pattern")]
-    public required string Pattern { get; set; }
-
-    [JsonConverter(typeof(PolicyTargetConverter))]
-    [JsonPropertyName("apply-to")]
-    public required PolicyTarget ApplyTo { get; set; }
-
-    [JsonPropertyName("definition")]
-    public required PolicyDefinition Definition { get; set; }
-
-    [JsonPropertyName("priority")]
-    public int Priority { get; set; }
+    [JsonConverter(typeof(FeatureFlagEnabledConverter))]
+    [JsonPropertyName("state")]
+    public bool IsEnabled { get; set; }
 
     [JsonExtensionData]
     public IDictionary<string, JsonElement> ExtraProperties { get; } = new Dictionary<string, JsonElement>();
