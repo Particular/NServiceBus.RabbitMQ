fa0e7477eb3f2c0b67323772a953fc6bf0cdbdf9
Andreas Bednarz <andreas.bednarz@particular.net>
2024-12-19 03:01:36 +1100

    Removed IManagementClient

--- src/NServiceBus.Transport.RabbitMQ/Administration/ManagementClient/ManagementClient.cs
+++ src/NServiceBus.Transport.RabbitMQ/Administration/ManagementClient/ManagementClient.cs
@@ -10,7 +10,7 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
-class ManagementClient : IManagementClient
+class ManagementClient
 {
     readonly HttpClient httpClient;
     readonly string virtualHost;
