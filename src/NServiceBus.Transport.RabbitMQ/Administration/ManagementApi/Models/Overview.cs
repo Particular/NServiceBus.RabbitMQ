8494cf46608d6c466509c414dcbd43af408fa239
Brandon Ording <bording@gmail.com>
2025-02-07 14:50:46 -0500

    Update Obsolete.Fody and remove workaround

--- src/NServiceBus.Transport.RabbitMQ/Administration/ManagementApi/Models/Overview.cs
+++ src/NServiceBus.Transport.RabbitMQ/Administration/ManagementApi/Models/Overview.cs
@@ -7,9 +7,6 @@ using System.Collections.Generic;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 
-// This is to prevent Fody throwing an error on classes with `required` properties (since the compiler marks the default constructor with an `[Obsolete]` attribute)
-// https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/proposals/csharp-11.0/required-members#metadata-representation
-[method: DoNotWarnAboutObsoleteUsage]
 class Overview()
 {
     [JsonPropertyName("product_name")]
