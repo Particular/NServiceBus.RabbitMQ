c80dd4bbacc3c5ecc0ecee1d70d1c595dc3bd183
Brandon Ording <bording@gmail.com>
2025-02-14 18:04:37 -0500

    Adjust ManagementClient and tests

---
+++ src/NServiceBus.Transport.RabbitMQ/Administration/ManagementApi/Models/FeatureFlagExtensions.cs
@@ -0,0 +1,14 @@
+﻿#nullable enable
+
+namespace NServiceBus.Transport.RabbitMQ.ManagementApi;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+static class FeatureFlagExtensions
+{
+    public static bool HasEnabledFeature(this List<FeatureFlag> list, string featureName) =>
+        list.Any(feature => feature.Name.Equals(featureName, StringComparison.OrdinalIgnoreCase) && feature.Enabled);
+}
+
--- src/NServiceBus.Transport.RabbitMQ/Administration/ManagementApi/Models/FeatureFlags.cs
+++ src/NServiceBus.Transport.RabbitMQ/Administration/ManagementApi/Models/FeatureFlags.cs
@@ -4,14 +4,6 @@ namespace NServiceBus.Transport.RabbitMQ.ManagementApi;
 
 static class FeatureFlags
 {
-    public const string DetailedQueuesEndpoints = "detailed_queues_endpoint";
-
-    public const string KhepriDatabase = "khepri_db";
-
-    public const string QuorumQueue = "quorum_queue";
-
-    public const string StreamFiltering = "stream_filtering";
-
     public const string StreamQueue = "stream_queue";
 }
 
