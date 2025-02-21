3299bbb8e8b077807e7219390f9e8741fe060f05
Travis Nickels <travis.nickels@particular.net>
2025-02-21 12:51:37 -0800

    Update ManagementClient for ServiceControl
    * Add/Update models
    
    * Add more API calls for ServiceControl
    
    * Add management client API tests

--- src/NServiceBus.Transport.RabbitMQ/Administration/ManagementApi/Models/Overview.cs
+++ src/NServiceBus.Transport.RabbitMQ/Administration/ManagementApi/Models/Overview.cs
@@ -8,5 +8,14 @@ class Overview()
 {
     [JsonPropertyName("rabbitmq_version")]
     public required string BrokerVersion { get; set; }
+
+    [JsonPropertyName("cluster_name")]
+    public required string ClusterName { get; set; }
+
+    [JsonPropertyName("node")]
+    public required string Node { get; set; }
+
+    [JsonPropertyName("disable_stats")]
+    public required bool DisableStats { get; set; }
 }
 
